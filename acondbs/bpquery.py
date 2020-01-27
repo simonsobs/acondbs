052b92648f5b84617bd9b67cc6ca13c896d03dff
Tai Sakuma <tai.sakuma@gmail.com>
2020-01-27 11:30:25 -0500

    add docstrings

--- acondbs/bpquery.py
+++ acondbs/bpquery.py
@@ -9,6 +9,9 @@ bp = Blueprint('query', __name__)
 ##__________________________________________________________________||
 @bp.route('/tables')
 def tables():
+    """returns all tables in HTML (to be deleted)
+    """
+
     table_names = ['maps', 'beams', 'map_path']
     tables = { }
     for table_name in table_names:
@@ -20,6 +23,9 @@ def tables():
 ##__________________________________________________________________||
 @bp.route('/query', methods=['POST'])
 def query():
+    """returns the results of the query in HTML (to be deleted)
+    """
+
     query = request.form['query']
     table_html = query_to_table_html(query)
     return jsonify(result=table_html)
@@ -27,6 +33,9 @@ def query():
 ##__________________________________________________________________||
 @bp.route('/maps')
 def maps():
+    """returns the content of the table maps in JSON
+    """
+
     query_ = "SELECT * FROM maps"
     return query_to_table_json(query_)
 
@@ -53,7 +62,29 @@ def query_to_table_html(query):
 ##__________________________________________________________________||
 def query_to_table_json(query):
     df = query_to_dataframe(query)
+
     json = df.to_json(orient='table', index=False)
+    # Note: the orient 'table' option is described in
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_json.html
+    # e.g.,
+    # >>> df = pd.DataFrame(dict(A=[10, 20], B=["abc", "def"]))
+    # >>> df
+    #     A    B
+    # 0  10  abc
+    # 1  20  def
+    # >>> df.to_json(orient='table', index=False)
+    # '{"schema":
+    #    {"fields": [
+    #         {"name":"A", "type":"integer"},
+    #         {"name":"B", "type":"string"}
+    #       ],
+    #     "pandas_version":"0.20.0"},
+    #     "data": [
+    #         {"A":10, "B":"abc"},
+    #         {"A":20, "B":"def"}
+    #       ]
+    # }'
+
     return json
 
 ##__________________________________________________________________||
