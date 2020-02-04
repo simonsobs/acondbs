3df4dd86c86dc0a1b2cdaf8b732fe2d0be7b2913
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-04 11:28:52 -0500

    add test_db.py

---
+++ tests/test_db.py
@@ -0,0 +1,32 @@
+import pytest
+import unittest.mock as mock
+
+import sqlalchemy
+
+from acondbs.db import get_db
+
+##__________________________________________________________________||
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlalchemy.exc.StatementError) as e:
+        db.execute("SELECT 1")
+
+    assert "closed" in str(e.value)
+
+##__________________________________________________________________||
+@pytest.fixture()
+def mock_init_db(monkeypatch):
+    ret = mock.Mock()
+    monkeypatch.setattr("acondbs.db.init_db", ret)
+    return ret
+
+##__________________________________________________________________||
+def test_init_db_command(runner, mock_init_db):
+    result = runner.invoke(args=["init-db"])
+    assert [mock.call()] == mock_init_db.call_args_list
+    assert "Initialized" in result.output
+
+##__________________________________________________________________||
