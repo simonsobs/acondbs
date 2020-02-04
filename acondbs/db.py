9cec4a73792343cc172a2f99fc9b342d83df2370
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-04 13:45:48 -0500

    add init_db(), init_db_command()

--- acondbs/db.py
+++ acondbs/db.py
@@ -1,5 +1,6 @@
 import click
 from flask import current_app, g
+from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
 
 ##__________________________________________________________________||
@@ -20,6 +21,19 @@ def close_db(e=None):
 ##__________________________________________________________________||
 def init_app(app):
     db.init_app(app)
+    app.cli.add_command(init_db_command)
     app.teardown_appcontext(close_db)
+
+##__________________________________________________________________||
+def init_db():
+    pass
+
+##__________________________________________________________________||
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
 
 ##__________________________________________________________________||
