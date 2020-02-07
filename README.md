fee3b1b595f59cb14c9f83316e47274417fc8833
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-07 15:33:29 -0500

    add README.md and setup.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# acondbs
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import versioneer
+
+import os
+import io
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='acondbs',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Backend server for Acondb',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Simons Observatory',
+    author_email='so_software@simonsobservatory.org',
+    url='https://github.com/simonsobs/acondbs',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=find_packages(exclude=['docs', 'tests'])
+)
