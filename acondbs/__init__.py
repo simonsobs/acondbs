e00c5d5aa92986414f992e9d70bc3703160373ef
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-07 15:33:49 -0500

    use versioneer

--- acondbs/__init__.py
+++ acondbs/__init__.py
@@ -43,3 +43,7 @@ def create_app(config_path=None):
     return app
 
 ##__________________________________________________________________||
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
