44647c837811db1c1a9776ec2e7d9ef030b992d0
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-07 16:10:40 -0500

    add LICENSE

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
