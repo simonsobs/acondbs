2f4dc3e94727442eca62bc267178526f8dd6847c
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-20 14:35:37 -0500

    add test_example_delete.py

---
+++ tests/models/test_example_delete.py
@@ -0,0 +1,31 @@
+from acondbs.db import db
+from acondbs.models import Map, Beam
+
+# These tests are written primarily for the developer to understand
+# how models in flask_sqlalchemy work.
+
+# __________________________________________________________________||
+def test_simple(app):
+    '''A simple test of deleting an object
+    '''
+
+    with app.app_context():
+
+        # save the initial number of the maps to compare later
+        nmaps = len(Map.query.all())
+
+    with app.app_context():
+        map1 = Map.query.filter_by(map_id=1012).first()
+        db.session.delete(map1)
+        db.session.commit()
+
+    with app.app_context():
+
+        # test the number of the maps is decreased by one
+        assert (nmaps - 1) == len(Map.query.all())
+
+        # the map is no longer found
+        map1 = Map.query.filter_by(map_id=1012).first()
+        assert map1 is None
+
+# __________________________________________________________________||
