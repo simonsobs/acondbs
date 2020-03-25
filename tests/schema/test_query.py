d24d5991a72625db2dc3380e33f2f6beedcdcd28
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-25 14:17:17 -0400

    update test for simulation

--- tests/schema/test_query.py
+++ tests/schema/test_query.py
@@ -7,6 +7,26 @@ from acondbs.schema.schema import schema
 params = [
     pytest.param(
         '''
+        { allSimulations {
+             edges { node { name } }
+           } }
+         ''',
+        id='allSimulations'
+    ),
+    pytest.param(
+        '''
+        { simulation(simulationId: 1001) { name } }
+         ''',
+        id='simulationBySimulationID'
+    ),
+    pytest.param(
+        '''
+        { simulation(simulationId: 2001) { name } }
+         ''',
+        id='simulationBySimulationID-nonexistent'
+    ),
+    pytest.param(
+        '''
         { allMaps(first: 2) {
              edges { node { name } }
            } }
