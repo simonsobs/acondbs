7d1e4415c35dbe0cc70b6732cecdaedf67bba146
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-26 12:19:29 -0400

    add docstrings

--- acondbs/db/db.py
+++ acondbs/db/db.py
@@ -1,3 +1,13 @@
+"""DB
+
+This module is similar to `db.py` in `falskr`, an example application
+described in the flask tutorial.
+
+ The flask tutorial: https://flask.palletsprojects.com/en/1.1.x/tutorial/
+
+ db.py in flaskr: https://github.com/pallets/flask/blob/1.1.1/examples/tutorial/flaskr/db.py
+
+"""
 import os
 import datetime
 import json
@@ -12,21 +22,45 @@ import sqlalchemy
 
 ##__________________________________________________________________||
 db = SQLAlchemy()
+"""the instance of SQLAlchemy
+
+It is named `db` because that appears to be common practice in Flask +
+SQLAlchemy applications.
+
+"""
 
 ##__________________________________________________________________||
 def get_db_connection():
+    """returns the DB connection
+
+    This function returns the `connection` of SQLAlchemy:
+    https://docs.sqlalchemy.org/en/13/core/connections.html
+
+    This function is similar to `get_db()` in `falskr`.
+
+    """
     if 'db_connection' not in g:
         g.db_connection = db.engine.connect()
     return g.db_connection
 
 ##__________________________________________________________________||
 def close_db_connection(e=None):
+    """closes the DB connection
+
+    This function is registered to be called when the application ends.
+
+    This function is similar to `close_db()` in `falskr`.
+    """
     conn = g.pop('db_connection', None)
     if conn is not None:
         conn.close()
 
 ##__________________________________________________________________||
 def init_app(app):
+    """initializes the application object
+
+    This function is called by `create_app()`
+    """
     db.init_app(app)
     app.cli.add_command(init_db_command)
     app.cli.add_command(dump_db_command)
