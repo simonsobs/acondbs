7203cb053be3b7459f4b9101dc2395fa55b2b315
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-26 16:55:49 -0400

    update docstring

--- acondbs/db/sa.py
+++ acondbs/db/sa.py
@@ -1,9 +1,18 @@
-"""the instance of SQLAlchemy
+"""the instance of SQLAlchemy from Flask-SQLAlchemy
 
 """
 from flask_sqlalchemy import SQLAlchemy
 
 ##__________________________________________________________________||
 sa = SQLAlchemy()
+"""the instance of SQLAlchemy from Flask-SQLAlchemy
 
+The instance is named "sa" here. However, it is more commonly named
+"db" in example code found online.
+
+- API: https://flask-sqlalchemy.palletsprojects.com/en/2.x/api/#flask_sqlalchemy.SQLAlchemy
+- The difference from the plain SQLAlchemy:
+  https://flask-sqlalchemy.palletsprojects.com/en/2.x/api/#flask_sqlalchemy.SQLAlchemy
+
+"""
 ##__________________________________________________________________||
