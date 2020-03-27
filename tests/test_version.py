a841e6bac951e3bd750ebe3c058c239039d529ae
Tai Sakuma <tai.sakuma@gmail.com>
2020-03-27 19:26:08 -0400

    add docstrings

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,6 +2,8 @@ import acondbs
 
 ##__________________________________________________________________||
 def test_version():
+    """test if the version string is attached to the module
+    """
     acondbs.__version__
 
 ##__________________________________________________________________||
