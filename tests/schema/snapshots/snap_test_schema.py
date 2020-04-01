7fe4182310475dcc1bef16cb5267d174fbf20692
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-01 14:10:50 -0400

    add test_schema.py

---
+++ tests/schema/snapshots/snap_test_schema.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_object[Simulation] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'simulationId'
+                },
+                {
+                    'name': 'name'
+                },
+                {
+                    'name': 'datePosted'
+                },
+                {
+                    'name': 'mapper'
+                },
+                {
+                    'name': 'note'
+                },
+                {
+                    'name': 'id'
+                }
+            ],
+            'name': 'Simulation'
+        }
+    }
+}
+
+snapshots['test_object[SimulationConnection] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'pageInfo'
+                },
+                {
+                    'name': 'edges'
+                }
+            ],
+            'name': 'SimulationConnection'
+        }
+    }
+}
+
+snapshots['test_object[Map] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'mapId'
+                },
+                {
+                    'name': 'name'
+                },
+                {
+                    'name': 'datePosted'
+                },
+                {
+                    'name': 'mapper'
+                },
+                {
+                    'name': 'note'
+                },
+                {
+                    'name': 'beams'
+                },
+                {
+                    'name': 'mapFilePaths'
+                },
+                {
+                    'name': 'id'
+                }
+            ],
+            'name': 'Map'
+        }
+    }
+}
+
+snapshots['test_object[MapConnection] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'pageInfo'
+                },
+                {
+                    'name': 'edges'
+                }
+            ],
+            'name': 'MapConnection'
+        }
+    }
+}
+
+snapshots['test_object[MapFilePath] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'mapFilePathId'
+                },
+                {
+                    'name': 'mapId'
+                },
+                {
+                    'name': 'path'
+                },
+                {
+                    'name': 'note'
+                },
+                {
+                    'name': 'map'
+                },
+                {
+                    'name': 'id'
+                }
+            ],
+            'name': 'MapFilePath'
+        }
+    }
+}
+
+snapshots['test_object[MapFilePathConnection] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'pageInfo'
+                },
+                {
+                    'name': 'edges'
+                }
+            ],
+            'name': 'MapFilePathConnection'
+        }
+    }
+}
+
+snapshots['test_object[Beam] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'beamId'
+                },
+                {
+                    'name': 'name'
+                },
+                {
+                    'name': 'path'
+                },
+                {
+                    'name': 'inputMapId'
+                },
+                {
+                    'name': 'inputBeamId'
+                },
+                {
+                    'name': 'map'
+                },
+                {
+                    'name': 'parentBeam'
+                },
+                {
+                    'name': 'childBeams'
+                },
+                {
+                    'name': 'id'
+                }
+            ],
+            'name': 'Beam'
+        }
+    }
+}
+
+snapshots['test_object[BeamConnection] 1'] = {
+    'data': {
+        '__type': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'pageInfo'
+                },
+                {
+                    'name': 'edges'
+                }
+            ],
+            'name': 'BeamConnection'
+        }
+    }
+}
---
+++ tests/schema/test_schema.py
@@ -0,0 +1,34 @@
+import pytest
+from graphene.test import Client
+
+from acondbs.schema.schema import schema
+
+##__________________________________________________________________||
+params = [
+    'Simulation', 'SimulationConnection',
+    'Map', 'MapConnection',
+    'MapFilePath', 'MapFilePathConnection',
+    'Beam', 'BeamConnection',
+]
+
+@pytest.mark.parametrize('type_name', params)
+def test_object(app, snapshot, type_name):
+    '''test objects defined
+    '''
+    query = '''
+      query q($type: String!) {
+        __type(name: $type) {
+          name
+          description
+          fields {
+            name
+          }
+        }
+      }
+    '''
+    variables = {'type': type_name}
+    client = Client(schema)
+    with app.app_context():
+        snapshot.assert_match(client.execute(query, variables=variables))
+
+##__________________________________________________________________||
