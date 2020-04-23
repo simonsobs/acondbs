7859f0736b4eb36843bb9f338929d4f331a0bdba
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-23 14:17:59 -0400

    add backup_db()

---
+++ acondbs/db/backup.py
@@ -0,0 +1,20 @@
+"""Backup DB
+"""
+from pathlib import Path
+
+from acondbs.db.ops import export_db_to_csv_files
+from acondbs.misc import gitb
+
+##__________________________________________________________________||
+def backup_db(repo_path):
+
+    repo_path = Path(repo_path)
+
+    for csv_file in repo_path.glob('*.csv'):
+        csv_file.unlink()
+
+    export_db_to_csv_files(repo_path)
+    gitb.commit(repo_path)
+    gitb.push(repo_path)
+
+##__________________________________________________________________||
