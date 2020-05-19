93b763777506c5c4ca1fe6435ee9cecbc909161b
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-19 16:52:10 -0400

    rewrite tests/models/test_example_*.py with new models

--- tests/models/test_example_delete.py
+++ tests/models/test_example_delete.py
@@ -1,5 +1,5 @@
 from acondbs.db.sa import sa
-from acondbs.models import Map, Beam
+from acondbs.models import Product
 
 # These tests are written primarily for the developer to understand
 # how models in flask_sqlalchemy work.
@@ -11,21 +11,21 @@ def test_simple(app):
 
     with app.app_context():
 
-        # save the initial number of the maps to compare later
-        nmaps = len(Map.query.all())
+        # save the initial number of the products to compare later
+        nproducts = len(Product.query.all())
 
     with app.app_context():
-        map1 = Map.query.filter_by(product_id=1012).first()
-        sa.session.delete(map1)
+        product1 = Product.query.filter_by(product_id=1012).first()
+        sa.session.delete(product1)
         sa.session.commit()
 
     with app.app_context():
 
-        # test the number of the maps is decreased by one
-        assert (nmaps - 1) == len(Map.query.all())
+        # test the number of the products is decreased by one
+        assert (nproducts - 1) == len(Product.query.all())
 
-        # the map is no longer found
-        map1 = Map.query.filter_by(product_id=1012).first()
-        assert map1 is None
+        # the product is no longer found
+        product1 = Product.query.filter_by(product_id=1012).first()
+        assert product1 is None
 
 # __________________________________________________________________||
