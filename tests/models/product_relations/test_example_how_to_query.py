5aafe08668c4f496e10803ddbcc805caaf5cea7e
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-25 12:59:50 -0400

    reformat code in a test

--- tests/models/product_relations/test_example_how_to_query.py
+++ tests/models/product_relations/test_example_how_to_query.py
@@ -23,15 +23,33 @@ def test_example_how_to_query(app):
         child2 = Product.query.filter_by(name='child2').first()
 
         # Product relations with the type name "child"
-        relations = ProductRelation.query.join(ProductRelationType).filter(ProductRelationType.name=='child').all()
+        relations = ProductRelation.query.join(
+            ProductRelationType
+        ).filter(
+            ProductRelationType.name=='child'
+        ).all()
         assert ['child', 'child'] == [r.type_.name for r in relations]
 
         # Products with "parent"
-        products = Product.query.join(ProductRelation, (Product.product_id == ProductRelation.self_product_id)).join(ProductRelationType).filter(ProductRelationType.name=='parent').all()
+        products = Product.query.join(
+            ProductRelation,
+            (Product.product_id == ProductRelation.self_product_id)
+        ).join(
+            ProductRelationType
+        ).filter(
+            ProductRelationType.name=='parent'
+        ).all()
         assert [child1, child2] == products
 
         # Products with "child"
-        products = Product.query.join(ProductRelation, (Product.product_id == ProductRelation.self_product_id)).join(ProductRelationType).filter(ProductRelationType.name=='child').all()
+        products = Product.query.join(
+            ProductRelation,
+            (Product.product_id == ProductRelation.self_product_id)
+        ).join(
+            ProductRelationType
+        ).filter(
+            ProductRelationType.name=='child'
+        ).all()
         assert [parent1] == products
 
 # __________________________________________________________________||
