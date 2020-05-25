a4b1d797f2e5cc5493b1d5fc170daf0363d24afe
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-25 12:51:14 -0400

    split test_product_relations.py into three files

--- tests/models/product_relations/test_cascade_delete.py
+++ tests/models/product_relations/test_cascade_delete.py
@@ -1,56 +1,5 @@
-import pytest
-
 from acondbs.db.sa import sa
 from acondbs.models import Product, ProductRelation, ProductRelationType
-
-# __________________________________________________________________||
-def test_relations(app):
-
-    with app.app_context():
-        parent1 = Product.query.filter_by(name='parent1').first()
-        child1 = Product.query.filter_by(name='child1').first()
-        child2 = Product.query.filter_by(name='child2').first()
-
-        assert 2 == len(parent1.relations)
-        assert 1 == len(child1.relations)
-        assert 1 == len(child2.relations)
-
-        assert 'child' == parent1.relations[0].type_.name
-        assert 'child' == parent1.relations[1].type_.name
-        assert 'parent' == child1.relations[0].type_.name
-        assert 'parent' == child2.relations[0].type_.name
-
-        assert child1 is parent1.relations[0].other
-        assert child2 is parent1.relations[1].other
-
-        assert parent1 is child1.relations[0].other
-        assert parent1 is child2.relations[0].other
-
-        assert parent1.relations[0] is child1.relations[0].reverse
-        assert parent1.relations[0].reverse is child1.relations[0]
-
-        assert parent1.relations[1] is child2.relations[0].reverse
-        assert parent1.relations[1].reverse is child2.relations[0]
-
-def test_example_how_to_query(app):
-
-    with app.app_context():
-
-        parent1 = Product.query.filter_by(name='parent1').first()
-        child1 = Product.query.filter_by(name='child1').first()
-        child2 = Product.query.filter_by(name='child2').first()
-
-        # Product relations with the type name "child"
-        relations = ProductRelation.query.join(ProductRelationType).filter(ProductRelationType.name=='child').all()
-        assert ['child', 'child'] == [r.type_.name for r in relations]
-
-        # Products with "parent"
-        products = Product.query.join(ProductRelation, (Product.product_id == ProductRelation.self_product_id)).join(ProductRelationType).filter(ProductRelationType.name=='parent').all()
-        assert [child1, child2] == products
-
-        # Products with "child"
-        products = Product.query.join(ProductRelation, (Product.product_id == ProductRelation.self_product_id)).join(ProductRelationType).filter(ProductRelationType.name=='child').all()
-        assert [parent1] == products
 
 # __________________________________________________________________||
 def test_cascade_delete_children(app):
