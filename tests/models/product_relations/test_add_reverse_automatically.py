773509d1e53719ec04d37bfd633aedffccb597e0
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-25 13:53:27 -0400

    add test_self_reverse_type()

--- tests/models/product_relations/test_add_reverse_automatically.py
+++ tests/models/product_relations/test_add_reverse_automatically.py
@@ -67,3 +67,58 @@ def test_permutations(app_empty, perm):
         assert parent1.relations[0].reverse is child1.relations[0]
 
 # __________________________________________________________________||
+@pytest.mark.parametrize('perm', params, ids=ids)
+def test_self_reverse_type(app_empty, perm):
+
+    app = app_empty
+
+    #
+    # +----------+                 +----------+
+    # |          |  --(sibling)--> |          |
+    # | sibling1 |       |         | sibling2 |
+    # |          |  <-(sibling)--  |          |
+    # +----------+                 +----------+
+    #
+
+    relation_type_sibling = ProductRelationType(name='sibling')
+    relation_type_sibling.reverse = relation_type_sibling
+
+    sibling1 = Product(name="sibling1")
+    sibling2 = Product(name="sibling2")
+
+    relation_sibling1_to_sibling2 = ProductRelation()
+
+    # set 'type_', 'self_', 'other' in different orders
+    # each triggers an event in which a reverse is automatically set
+    for p in perm:
+        if p == 1:
+            relation_sibling1_to_sibling2.type_ = relation_type_sibling
+        elif p == 2:
+            relation_sibling1_to_sibling2.self_ = sibling1
+        elif p == 3:
+            relation_sibling1_to_sibling2.other = sibling2
+
+    with app.app_context():
+        sa.session.add(sibling1)
+        sa.session.commit()
+
+    with app.app_context():
+        sibling1 = Product.query.filter_by(name='sibling1').first()
+        sibling2 = Product.query.filter_by(name='sibling2').first()
+
+        assert 1 == len(sibling1.relations)
+        assert 1 == len(sibling2.relations)
+
+        assert 'sibling' == sibling1.relations[0].type_.name
+        assert 'sibling' == sibling2.relations[0].type_.name
+
+        assert sibling1.relations[0].type_ is sibling2.relations[0].type_
+
+        assert sibling2 is sibling1.relations[0].other
+
+        assert sibling1 is sibling2.relations[0].other
+
+        assert sibling1.relations[0] is sibling2.relations[0].reverse
+        assert sibling1.relations[0].reverse is sibling2.relations[0]
+
+# __________________________________________________________________||
