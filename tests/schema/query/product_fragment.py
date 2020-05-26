14e30a7868e346dcecffa9b4b8fa710ae144b768
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-25 23:22:10 -0400

    move productFragment to its own file

---
+++ tests/schema/query/product_fragment.py
@@ -0,0 +1,63 @@
+
+##__________________________________________________________________||
+productFragment = '''
+fragment productFragment on Product {
+  productId
+  typeId
+  type_ {
+    typeId
+    name
+  }
+  name
+  contact
+  dateProduced
+  producedBy
+  datePosted
+  postedBy
+  dateUpdated
+  updatedBy
+  paths {
+    edges {
+      node {
+        pathId
+        path
+        note
+      }
+    }
+  }
+  relations {
+    edges {
+      node {
+        relationId
+        typeId
+        type_ {
+          typeId
+          name
+        }
+        otherProductId
+        other {
+          productId
+          typeId
+          type_ {
+            typeId
+            name
+          }
+          name
+        }
+        reverseRelationId
+        reverse {
+          relationId
+          typeId
+          type_ {
+            typeId
+            name
+          }
+        }
+      }
+    }
+  }
+  note
+}
+'''
+
+##__________________________________________________________________||
--- tests/schema/query/test_product.py
+++ tests/schema/query/test_product.py
@@ -3,67 +3,7 @@ import textwrap
 
 from .funcs import assert_query_success
 
-productFragment = '''
-fragment productFragment on Product {
-  productId
-  typeId
-  type_ {
-    typeId
-    name
-  }
-  name
-  contact
-  dateProduced
-  producedBy
-  datePosted
-  postedBy
-  dateUpdated
-  updatedBy
-  paths {
-    edges {
-      node {
-        pathId
-        path
-        note
-      }
-    }
-  }
-  relations {
-    edges {
-      node {
-        relationId
-        typeId
-        type_ {
-          typeId
-          name
-        }
-        otherProductId
-        other {
-          productId
-          typeId
-          type_ {
-            typeId
-            name
-          }
-          name
-        }
-        reverseRelationId
-        reverse {
-          relationId
-          typeId
-          type_ {
-            typeId
-            name
-          }
-        }
-      }
-    }
-  }
-  note
-}
-'''
-
-print(productFragment)
+from .product_fragment import productFragment
 
 ##__________________________________________________________________||
 params = [
