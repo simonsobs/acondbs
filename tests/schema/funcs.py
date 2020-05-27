46e88ebf5d0f38242802a581eccb9d97af166e2e
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-27 16:26:40 -0400

    use if clause so pytest prints the content of result

--- tests/schema/funcs.py
+++ tests/schema/funcs.py
@@ -10,7 +10,10 @@ def assert_query(app, snapshot, query, error=False):
     client = Client(schema)
     with app.app_context():
         result = client.execute(*args, **kwags)
-    assert ('errors' in result) == error
+    if error:
+        assert 'errors' in result
+    else:
+        assert 'errors' not in result
     snapshot.assert_match(result)
 
 def assert_mutation(app, snapshot, mutation, query, mock_request_backup_db, success=True):
