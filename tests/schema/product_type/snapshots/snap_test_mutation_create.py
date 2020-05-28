7f78694d4e1df97fc2fcd17be76fbfc9faf74e08
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-28 13:53:14 -0400

    update tests for createProductType()

--- tests/schema/product_type/snapshots/snap_test_mutation_create.py
+++ tests/schema/product_type/snapshots/snap_test_mutation_create.py
@@ -7,33 +7,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_schema_success[create] 1'] = {
-    'data': {
-        'createProductType': {
-            'productType': {
-                'name': 'compass'
-            }
-        }
-    }
-}
-
-snapshots['test_schema_success[create] 2'] = {
-    'data': {
-        'productType': {
-            'icon': 'mdi-compass',
-            'indefArticle': 'a',
-            'name': 'compass',
-            'plural': 'compasses',
-            'products': {
-                'edges': [
-                ]
-            },
-            'singular': 'compass',
-            'typeId': '5'
-        }
-    }
-}
-
 snapshots['test_schema_error[error-already-exist] 1'] = {
     'data': {
         'createProductType': None
@@ -42,13 +15,13 @@ snapshots['test_schema_error[error-already-exist] 1'] = {
         {
             'locations': [
                 {
-                    'column': 17,
+                    'column': 3,
                     'line': 3
                 }
             ],
             'message': '''(sqlite3.IntegrityError) UNIQUE constraint failed: product_types.name
 [SQL: INSERT INTO product_types (name, "order", indef_article, singular, plural, icon) VALUES (?, ?, ?, ?, ?, ?)]
-[parameters: ('map', None, None, None, None, None)]
+[parameters: ('map', 5, 'a', 'map', 'maps', 'mdi-map')]
 (Background on this error at: http://sqlalche.me/e/gkpj)''',
             'path': [
                 'createProductType'
@@ -79,6 +52,61 @@ snapshots['test_schema_error[error-already-exist] 2'] = {
                 {
                     'node': {
                         'name': 'anchor'
+                    }
+                }
+            ]
+        }
+    }
+}
+
+snapshots['test_schema_success[create] 1'] = {
+    'data': {
+        'createProductType': {
+            'ok': True,
+            'productType': {
+                'icon': 'mdi-compass',
+                'indefArticle': 'a',
+                'name': 'compass',
+                'order': 5,
+                'plural': 'compasses',
+                'products': {
+                    'edges': [
+                    ]
+                },
+                'singular': 'compass',
+                'typeId': '5'
+            }
+        }
+    }
+}
+
+snapshots['test_schema_success[create] 2'] = {
+    'data': {
+        'allProductTypes': {
+            'edges': [
+                {
+                    'node': {
+                        'name': 'map'
+                    }
+                },
+                {
+                    'node': {
+                        'name': 'beam'
+                    }
+                },
+                {
+                    'node': {
+                        'name': 'simulation'
+                    }
+                },
+                {
+                    'node': {
+                        'name': 'anchor'
+                    }
+                },
+                {
+                    'node': {
+                        'name': 'compass'
                     }
                 }
             ]
--- tests/schema/product_type/test_mutation_create.py
+++ tests/schema/product_type/test_mutation_create.py
@@ -2,46 +2,37 @@ import pytest
 
 from ..funcs import assert_mutation
 
+from ..gql import CREATE_PRODUCT_TYPE
+
+QEURY = '''
+{
+  allProductTypes {
+    edges {
+      node {
+        name
+      }
+    }
+  }
+}
+'''
+
 ##__________________________________________________________________||
 params = [
     pytest.param(
         [
-            ['''
-              mutation m {
-                createProductType(input: {
-                  name: "compass",
-                  order: 5,
-                  indefArticle: "a",
-                  singular: "compass",
-                  plural: "compasses",
-                  icon: "mdi-compass"
-                }) { productType { name } }
-              }
-            '''],
-            {},
-        ],
-        [
-            ['''
-              {
-                productType(name: "compass") {
-                  typeId
-                  name
-                  indefArticle
-                  singular
-                  plural
-                  icon
-                  products {
-                    edges {
-                      node {
-                        name
-                      }
-                    }
-                  }
+            [CREATE_PRODUCT_TYPE],
+            {'variables': {
+                'input': {
+                  'name': "compass",
+                  'order': 5,
+                  'indefArticle': "a",
+                  'singular': "compass",
+                  'plural': "compasses",
+                  'icon': "mdi-compass"
                 }
-              }
-            '''],
-            {},
+            }},
         ],
+        [[QEURY], {}],
         id='create'
     ),
 ]
@@ -55,29 +46,19 @@ def test_schema_success(app, snapshot, mutation, query, mock_request_backup_db):
 params = [
     pytest.param(
         [
-            ['''
-              mutation m {
-                createProductType(input: {
-                  name: "map",
-                }) { productType { name } }
-              }
-            '''],
-            {}
-        ],
-        [
-            ['''
-              {
-                allProductTypes {
-                  edges {
-                    node {
-                      name
-                    }
-                  }
+            [CREATE_PRODUCT_TYPE],
+            {'variables': {
+                'input': {
+                  'name': "map",
+                  'order': 5,
+                  'indefArticle': "a",
+                  'singular': "map",
+                  'plural': "maps",
+                  'icon': "mdi-map"
                 }
-              }
-            ''',],
-            {}
+            }},
         ],
+        [[QEURY], {}],
         id='error-already-exist'
     ),
 ]
