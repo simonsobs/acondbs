8bf3bf7837bef81809c88cc951cd264ae2ee5cae
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-29 18:28:35 -0400

    add a wrapper class GraphQLViewW

--- acondbs/bpgraphql.py
+++ acondbs/bpgraphql.py
@@ -13,14 +13,42 @@ from .schema.schema import schema
 ##__________________________________________________________________||
 bp = Blueprint('graphql', __name__)
 
-bp.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view(
-        'graphql',
-        schema=schema, graphiql=True, ))
+schema_holder = []
+
+class GraphQLViewW(GraphQLView):
+    '''A wrapper.
+
+    used to create schema in init_app() where app.config is accessible.
+
+
+    The usual usage of GraphQLView in add_url_rule() as in the document
+    (https://github.com/graphql-python/flask-graphql/tree/v2.0.1#usage) is as
+    follows
+      bp.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+    The schema needs to be defined in the global scope.
+
+    In this app, the schema is not available globally because its creation
+    depends on the configuration. The configuration is not available globally.
+    It is only available in the app context. Therefore, the schema is created
+    in init_app(). Then it is place in the schema_holder.
+
+    The __init__() will be called in each view from
+    https://github.com/pallets/flask/blob/1.1.2/src/flask/views.py#L88
+
+    In __init__(), schema is retrieved from schema_holder and is given to
+    __init__() of GraphQLView.
+
+    '''
+    def __init__(self, **kwargs):
+        kwargs['schema'] = schema_holder[0]
+        super().__init__(**kwargs)
+
+
+bp.add_url_rule('/graphql', view_func=GraphQLViewW.as_view('graphql', graphiql=True))
 
 ##__________________________________________________________________||
 def init_app(app):
+    schema_holder[:] = [schema]
     app.register_blueprint(bp)
 
 ##__________________________________________________________________||
