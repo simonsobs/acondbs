2a69f40ebcbb39a0d65e4c65b2532d2d84906777
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-29 20:27:41 -0400

    add test_disable_mutation()

---
+++ tests/snapshots/snap_test_bpgraphql.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['test_disable_mutation[kwargs0] 1'] = {
+    'data': {
+        'Mutation': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'createProduct'
+                },
+                {
+                    'name': 'updateProduct'
+                },
+                {
+                    'name': 'deleteProduct'
+                },
+                {
+                    'name': 'createProductFilePath'
+                },
+                {
+                    'name': 'updateProductFilePath'
+                },
+                {
+                    'name': 'deleteProductFilePath'
+                },
+                {
+                    'name': 'createProductType'
+                },
+                {
+                    'name': 'updateProductType'
+                },
+                {
+                    'name': 'deleteProductType'
+                },
+                {
+                    'name': 'createProductRelationTypes'
+                },
+                {
+                    'name': 'updateProductRelationType'
+                },
+                {
+                    'name': 'deleteProductRelationTypes'
+                },
+                {
+                    'name': 'createProductRelation'
+                },
+                {
+                    'name': 'deleteProductRelation'
+                }
+            ],
+            'name': 'Mutation'
+        },
+        'product': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'version'
+                },
+                {
+                    'name': 'node'
+                },
+                {
+                    'name': 'allProductTypes'
+                },
+                {
+                    'name': 'productType'
+                },
+                {
+                    'name': 'allProducts'
+                },
+                {
+                    'name': 'allProductFilePaths'
+                },
+                {
+                    'name': 'product'
+                },
+                {
+                    'name': 'allProductRelationTypes'
+                },
+                {
+                    'name': 'productRelationType'
+                },
+                {
+                    'name': 'allProductRelations'
+                },
+                {
+                    'name': 'productRelation'
+                }
+            ],
+            'name': 'Query'
+        }
+    }
+}
+
+snapshots['test_disable_mutation[kwargs1] 1'] = {
+    'data': {
+        'Mutation': None,
+        'product': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'version'
+                },
+                {
+                    'name': 'node'
+                },
+                {
+                    'name': 'allProductTypes'
+                },
+                {
+                    'name': 'productType'
+                },
+                {
+                    'name': 'allProducts'
+                },
+                {
+                    'name': 'allProductFilePaths'
+                },
+                {
+                    'name': 'product'
+                },
+                {
+                    'name': 'allProductRelationTypes'
+                },
+                {
+                    'name': 'productRelationType'
+                },
+                {
+                    'name': 'allProductRelations'
+                },
+                {
+                    'name': 'productRelation'
+                }
+            ],
+            'name': 'Query'
+        }
+    }
+}
+
+snapshots['test_disable_mutation[kwargs2] 1'] = {
+    'data': {
+        'Mutation': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'createProduct'
+                },
+                {
+                    'name': 'updateProduct'
+                },
+                {
+                    'name': 'deleteProduct'
+                },
+                {
+                    'name': 'createProductFilePath'
+                },
+                {
+                    'name': 'updateProductFilePath'
+                },
+                {
+                    'name': 'deleteProductFilePath'
+                },
+                {
+                    'name': 'createProductType'
+                },
+                {
+                    'name': 'updateProductType'
+                },
+                {
+                    'name': 'deleteProductType'
+                },
+                {
+                    'name': 'createProductRelationTypes'
+                },
+                {
+                    'name': 'updateProductRelationType'
+                },
+                {
+                    'name': 'deleteProductRelationTypes'
+                },
+                {
+                    'name': 'createProductRelation'
+                },
+                {
+                    'name': 'deleteProductRelation'
+                }
+            ],
+            'name': 'Mutation'
+        },
+        'product': {
+            'description': None,
+            'fields': [
+                {
+                    'name': 'version'
+                },
+                {
+                    'name': 'node'
+                },
+                {
+                    'name': 'allProductTypes'
+                },
+                {
+                    'name': 'productType'
+                },
+                {
+                    'name': 'allProducts'
+                },
+                {
+                    'name': 'allProductFilePaths'
+                },
+                {
+                    'name': 'product'
+                },
+                {
+                    'name': 'allProductRelationTypes'
+                },
+                {
+                    'name': 'productRelationType'
+                },
+                {
+                    'name': 'allProductRelations'
+                },
+                {
+                    'name': 'productRelation'
+                }
+            ],
+            'name': 'Query'
+        }
+    }
+}
--- tests/test_bpgraphql.py
+++ tests/test_bpgraphql.py
@@ -12,3 +12,51 @@ def test_graphql(client):
     # print(json.dumps(un_jsonified, indent=2))
 
 ##__________________________________________________________________||
+from acondbs import create_app
+from .constants import SAMPLE_DIR
+from pathlib import Path
+from acondbs.db.ops import define_tables
+
+params = [
+    {},
+    {'ACONDBS_SCHEME_MUTATION_DISABLE': True},
+    {'ACONDBS_SCHEME_MUTATION_DISABLE': False},
+]
+
+@pytest.mark.parametrize('kwargs', params)
+def test_disable_mutation(kwargs, snapshot):
+    config_path = Path(SAMPLE_DIR, 'config.py')
+    kwargs.update(dict(
+        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:"
+    ))
+    app = create_app(config_path, **kwargs)
+    with app.app_context():
+        define_tables()
+    client = app.test_client()
+
+    query = '''
+        {
+          product: __type(name: "Query") {
+            name
+            description
+            fields {
+              name
+            }
+          }
+          Mutation: __type(name: "Mutation") {
+            name
+            description
+            fields {
+              name
+            }
+          }
+        }
+    '''
+
+    response = client.get(
+        '/graphql',
+        query_string=dict(query=query))
+    assert 200 == response.status_code
+    snapshot.assert_match(json.loads(response.data))
+
+##__________________________________________________________________||
