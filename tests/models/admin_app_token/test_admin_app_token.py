6d1c7492241bfe8ea7bbc1cf49ed304b38f090e5
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-09 15:49:28 -0500

    update tests for the model AdminAppToken

--- tests/models/admin_app_token/test_admin_app_token.py
+++ tests/models/admin_app_token/test_admin_app_token.py
@@ -9,18 +9,14 @@ from acondbs.models import AdminAppToken
 def test_token(app_empty):
     app = app_empty
 
-    token1 = AdminAppToken(token="token123")
+    row = AdminAppToken(token="token123")
 
     with app.app_context():
-        sa.session.add(token1)
+        sa.session.add(row)
         sa.session.commit()
 
     with app.app_context():
-        token1 = AdminAppToken.query.all()
-        print(token1[0].token)        
-        # assert map1 is not None
-        # type_map = map1.type_
-        # assert 'map' == type_map.name
-        # assert [map1] == type_map.products
+        row = AdminAppToken.query.one()
+        assert row.token == "token123"
 
 # __________________________________________________________________||
