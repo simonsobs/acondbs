d56191431774ff1c97982670fa43667a36cba4fd
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-15 16:47:04 -0500

    update a test config

--- tests/sample/config.py
+++ tests/sample/config.py
@@ -1,21 +1,28 @@
 from pathlib import Path
 
-_THISDIR = Path(__file__).resolve().parent
+TESTING = True
+SECRET_KEY='secret_key_test_123'
 
 ##__________________________________________________________________||
-TESTING = True
+_THISDIR = Path(__file__).resolve().parent
 
-SECRET_KEY='secret_key_test_123'
+ACONDBS_DB_FOLDER = _THISDIR
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(_THISDIR.joinpath('product.sqlite3'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-ACONDBS_DB_BACKUP_CSV_GIT_FOLDER = _THISDIR.joinpath('db-backup-csv')
 ACONDBS_DB_BACKUP_PAUSE = 1.0 # second
+
+ACONDBS_DB_BACKUP_LOCK = ACONDBS_DB_FOLDER.joinpath('.lock')
+ACONDBS_DB_BACKUP_LOCK_TIMEOUT = 2.0 # second
+
+ACONDBS_DB_BACKUP_CSV_GIT_FOLDER = _THISDIR.joinpath('db-backup-csv')
 ACONDBS_DB_BACKUP_CSV_GIT_LOCK = ACONDBS_DB_BACKUP_CSV_GIT_FOLDER.joinpath('.lock')
 ACONDBS_DB_BACKUP_CSV_GIT_LOCK_TIMEOUT = 2.0 # second
 
 ACONDBS_SCHEME_MUTATION_DISABLE = False
+
+del _THISDIR
 
 ##__________________________________________________________________||
 GITHUB_AUTH_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
@@ -33,7 +40,6 @@ GITHUB_AUTH_ADMIN_REDIRECT_URI = 'http://localhost:8080/admin/signin'
 GITHUB_ORG = 'test_github_org_name'
 
 ##__________________________________________________________________||
-del _THISDIR
 del Path
 
 ##__________________________________________________________________||
