51416043494c3f790255c5f5eb85bd1c349e3ed4
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-25 13:13:17 -0500

    add __repr__() to GitHubUser

--- acondbs/models/github/github_user.py
+++ acondbs/models/github/github_user.py
@@ -6,4 +6,7 @@ class GitHubUser(sa.Model):
     user_id = sa.Column(sa.Integer(), primary_key=True)
     login = sa.Column(sa.Text(), nullable=False)
 
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.login!r}>'
+
 ##__________________________________________________________________||
