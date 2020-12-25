79b69cabf33401010693831889e3d7be6542f5b1
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-25 15:44:44 -0500

    add naming conventions to the metadata of SQLAlchemy

--- acondbs/db/sa.py
+++ acondbs/db/sa.py
@@ -1,10 +1,24 @@
 """the instance of SQLAlchemy from Flask-SQLAlchemy
 
 """
+from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 
 ##__________________________________________________________________||
-sa = SQLAlchemy()
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/#using-custom-metadata-and-naming-conventions
+# https://stackoverflow.com/a/56000475/7309855
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    # "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+
+##__________________________________________________________________||
+sa = SQLAlchemy(metadata=metadata)
 """the instance of SQLAlchemy from Flask-SQLAlchemy
 
 The instance is named "sa" here. However, it is more commonly named
