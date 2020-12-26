6918f517a732f71ad43c28c60cdf1e924c81e75c
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-26 14:33:33 -0500

    make login unique in the table github_users

--- acondbs/models/github/github_user.py
+++ acondbs/models/github/github_user.py
@@ -4,7 +4,7 @@ from ...db.sa import sa
 class GitHubUser(sa.Model):
     __tablename__ = 'github_users'
     user_id = sa.Column(sa.Integer(), primary_key=True)
-    login = sa.Column(sa.Text(), nullable=False)
+    login = sa.Column(sa.Text(), unique=True, nullable=False)
 
     def __repr__(self):
         return f'<{self.__class__.__name__} {self.login!r}>'
