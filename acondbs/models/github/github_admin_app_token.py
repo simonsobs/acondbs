0ec85d8b54903bd5c0c67b3b2ba85d6826acf671
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-26 13:18:08 -0500

    rename the table github_admin_app_token github_tokens

--- acondbs/models/github/github_admin_app_token.py
+++ acondbs/models/github/github_admin_app_token.py
@@ -8,7 +8,7 @@ def encription_key():
 
 ##__________________________________________________________________||
 class GitHubAdminAppToken(sa.Model):
-    __tablename__ = 'github_admin_app_token'
+    __tablename__ = 'github_tokens'
     token_id = sa.Column(sa.Integer(), primary_key=True)
     token = sa.Column(EncryptedType(sa.Text(), key=encription_key))
     scope = sa.Column(sa.Text())
