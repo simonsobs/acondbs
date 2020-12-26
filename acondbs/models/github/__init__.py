597b014ab158aca3f18e64de06ad8fe9fb41e830
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-26 13:29:48 -0500

    rename the file github_admin_app_token.py github_token.py

--- acondbs/models/github/__init__.py
+++ acondbs/models/github/__init__.py
@@ -2,7 +2,7 @@
 """
 
 ##__________________________________________________________________||
-from .github_admin_app_token import GitHubToken
+from .github_token import GitHubToken
 from .github_accepted_org import GitHubAcceptedOrg
 from .github_user import GitHubUser
 from .github_org_membership import GitHubOrgMembership
