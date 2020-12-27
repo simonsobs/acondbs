723eeb926eddc48445066b0c678b119b12e1fa9c
Tai Sakuma <tai.sakuma@gmail.com>
2020-12-27 12:12:26 -0500

    update docstring, comments

--- acondbs/github/call.py
+++ acondbs/github/call.py
@@ -14,9 +14,9 @@ def call_graphql_api(query, variables=None, token=None):
     query : str
         A GraphQL query, e.g.,
         'query User($login: String!) { user(login: $login) { name } }'
-    variables: dict
+    variables: dict, optional
         Variables for the query, e.g., {"login": "octocat"}
-    token: str
+    token: str, optional
         An access token
 
     Returns
@@ -40,32 +40,37 @@ def call_graphql_api(query, variables=None, token=None):
     if variables:
         json['variables'] = variables
 
+    # example:
+    #   headers = {'Authorization': 'token XXXXXXXXXXXXXXXXXXXX'}
+    #   json = {
+    #       'query': (
+    #           'query User($login: String!) '
+    #           '{ user(login: $login) { name } }'
+    #       ),
+    #       'variables': {"login": "octocat"}
+    #   }
+
+
     response = requests.post(API_URL, json=json, headers=headers)
     response = response.json()
-    # Examples: query = '{ viewer { login name avatarUrl } }'
-    #   Success:
+    # examples:
+    #   success:
+    #     response = {"data": {"user": {"name": "The Octocat"} } }
+    #
+    #   error: (note: the 'data' field might not exist.)
     #     response = {
-    #         "data": {
-    #             "viewer": {
-    #                 "login": "octocat",
-    #                 "name": "The Octocat",
-    #                 "avatarUrl": "https://avatars3.githubusercontent.com/u/583231?u=a59fef2a493e2b67dd13754231daf220c82ba84d&v=4"
+    #         "data": {"user": null},
+    #         "errors": [
+    #             {
+    #                 "type": "NOT_FOUND",
+    #                 "path": ["user"],
+    #                 "locations": [{"line": 7, "column": 3 }],
+    #                 "message": "Could not resolve to a User with the login of 'octocat'."
     #             }
-    #         }
+    #         ]
     #     }
     #
-    #   Error: sometimes, the 'data' field still exists.
-    #     response = {
-    #         'errors': [
-    #             {
-    #                 'path': ['query', 'viewer', 'idii'],
-    #                 'extensions': {'code': 'undefinedField', 'typeName': 'User', 'fieldName': 'idii'},
-    #                 'locations': [{'line': 1, 'column': 12}],
-    #                 'message': "Field 'idii' doesn't exist on type 'User'"
-    #             }
-    #         ]}
-    #
-    #   Bad credentials:
+    #   bad credentials:
     #     response = {
     #         'message': 'Bad credentials',
     #         'documentation_url': 'https://docs.github.com/graphql'
