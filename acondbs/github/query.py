058331ceb719bf9dc51b5f315d37f6fb1bf74bc5
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-03 12:39:46 -0500

    add functions in github.query

--- acondbs/github/query.py
+++ acondbs/github/query.py
@@ -1,7 +1,121 @@
-"""Queries
+"""Queries to GitHub API
 """
 
 from .call import call_graphql_api
+
+##__________________________________________________________________||
+def org(login, token):
+    query = """
+      query Organization($login: String!) {
+        organization(login: $login) {
+          id
+          login
+        }
+      }
+    """
+    variables = { "login": login }
+    r = call_graphql_api(query=query, variables=variables, token=token)
+    # e.g.,
+    #   {
+    #     "organization": {
+    #       "id": "MDEyOk9yZ2FuaXphdGlvbjc1NjMxODQ0",
+    #       "login": "urban-octo-disco"
+    #     }
+    #   }
+
+    return r['organization']
+
+##__________________________________________________________________||
+def org_members(org_login, token):
+    query = """
+      query OrganizationMemberCount($org_login: String!) {
+        organization(login: $org_login) {
+          login
+          membersWithRole {
+            totalCount
+          }
+        }
+      }
+    """
+    variables = { "org_login": org_login }
+    r = call_graphql_api(query=query, variables=variables, token=token)
+    # e.g.,
+    # {
+    #     'organization': {
+    #         'login': 'urban-octo-disco',
+    #         'membersWithRole': {'totalCount': 2}
+    #     }
+    # }
+
+    nmembers = r['organization']['membersWithRole']['totalCount']
+
+    query = """
+      query OrganizationMembers($org_login: String!, $first: Int!) {
+        organization(login: $org_login) {
+          login
+          membersWithRole(first: $first) {
+            edges {
+              role
+              node {
+                id
+                login
+                name
+                avatarUrl
+              }
+            }
+          }
+        }
+      }
+    """
+    variables = { "org_login": org_login, "first": nmembers }
+    r = call_graphql_api(query=query, variables=variables, token=token)
+    # e.g.,
+    # {
+    #     'organization': {
+    #         'login': 'urban-octo-disco',
+    #         'membersWithRole': {
+    #             'edges': [
+    #                 {
+    #                     'node': {
+    #                         'id': 'MDQ6VXNlcjEzODgwODE=',
+    #                         'login': 'TaiSakuma',
+    #                         'name': 'Tai Sakuma',
+    #                         'avatarUrl': 'https://avatars0.githubusercontent.com/u/1388081?v=4'
+    #                     },
+    #                     'role': 'MEMBER'
+    #                 },
+    #                 {
+    #                     'node': {
+    #                         'id': 'MDQ6VXNlcjE1Njg1Njk3',
+    #                         'login': 'tai-sakuma',
+    #                         'name': None,
+    #                         'avatarUrl': 'https://avatars0.githubusercontent.com/u/15685697?v=4'
+    #                     },
+    #                     'role': 'ADMIN'
+    #                 }
+    #             ]}
+    #     }
+    # }
+
+    edges = r['organization']['membersWithRole']['edges']
+
+    return edges
+
+##__________________________________________________________________||
+def viewer(token):
+    query = '{ viewer { login id name avatarUrl } }'
+    r = call_graphql_api(query=query, token=token)
+    # e.g., https://github.com/octocat
+    # {
+    #     "viewer": {
+    #         "id": "MDQ6VXNlcjU4MzIzMQ==",
+    #         "login": "octocat",
+    #         "name": "The Octocat",
+    #         "avatarUrl": "https://avatars3.githubusercontent.com/u/583231?u=a59fef2a493e2b67dd13754231daf220c82ba84d&v=4"
+    #     }
+    # }
+    user = r.get('viewer')
+    return user
 
 ##__________________________________________________________________||
 def get_user(token):
