1a4385ffd122e00e4b5011ff9772c66323830d9a
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-04 14:32:27 -0500

    set default to time_created in GitHubToken

--- acondbs/models/github/github_token.py
+++ acondbs/models/github/github_token.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import current_app
 from sqlalchemy_utils import EncryptedType
 
@@ -20,6 +22,6 @@ class GitHubToken(sa.Model):
         "GitHubUser",
         backref=sa.backref("tokens", cascade="all, delete-orphan")
         )
-    time_created = sa.Column(sa.DateTime())
+    time_created = sa.Column(sa.DateTime(), default=lambda : datetime.datetime.now())
 
 ##__________________________________________________________________||
