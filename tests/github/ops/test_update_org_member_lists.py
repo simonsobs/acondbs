095b8ceb3a624311f744e579a48f9d5490a06dfb
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-04 11:02:20 -0500

    add a test file

---
+++ tests/github/ops/test_update_org_member_lists.py
@@ -0,0 +1,9 @@
+from acondbs.github.ops import update_org_member_lists
+
+##__________________________________________________________________||
+def test_call(app, snapshot):
+    with app.app_context():
+        # update_org_member_lists()
+        pass
+
+##__________________________________________________________________||
