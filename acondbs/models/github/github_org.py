f813181432acec642634d68d4e1cddfbfceeb84b
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-04 15:27:59 -0500

    add a field git_hub_id to GitHubOrg and GitHubUser

--- acondbs/models/github/github_org.py
+++ acondbs/models/github/github_org.py
@@ -4,6 +4,7 @@ from ...db.sa import sa
 class GitHubOrg(sa.Model):
     __tablename__ = 'github_orgs'
     org_id = sa.Column(sa.Integer(), primary_key=True)
+    git_hub_id = sa.Column(sa.Text(), unique=True, nullable=False)
     login = sa.Column(sa.Text(), nullable=False)
 
     def __repr__(self):
--- acondbs/models/github/github_user.py
+++ acondbs/models/github/github_user.py
@@ -4,6 +4,7 @@ from ...db.sa import sa
 class GitHubUser(sa.Model):
     __tablename__ = 'github_users'
     user_id = sa.Column(sa.Integer(), primary_key=True)
+    git_hub_id = sa.Column(sa.Text(), unique=True, nullable=False)
     login = sa.Column(sa.Text(), unique=True, nullable=False)
     name = sa.Column(sa.Text())
     avatar_url = sa.Column(sa.Text())
