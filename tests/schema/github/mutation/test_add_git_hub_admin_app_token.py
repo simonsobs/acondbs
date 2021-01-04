05e4c9f9884341abcd6794fa29e88db6c24d646a
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-04 11:02:56 -0500

    update test for addGitHubAdminAppToken

--- tests/schema/github/mutation/test_add_git_hub_admin_app_token.py
+++ tests/schema/github/mutation/test_add_git_hub_admin_app_token.py
@@ -27,34 +27,10 @@ ALL_GITHUB_ADMIN_APP_TOKENS = '''
 '''
 
 ##__________________________________________________________________||
-@pytest.fixture()
-def mock_auth_requests_success(monkeypatch):
-    y = mock.Mock()
-    monkeypatch.setattr("acondbs.github.call.requests", y)
-    response = {'access_token': 'token-xxx', 'token_type': 'bearer', 'scope': 'user'}
-    r = mock.Mock()
-    r.json.return_value = response
-    y.post.return_value = r
-    yield y
-
-@pytest.fixture()
-def mock_auth_requests_error(monkeypatch):
-    y = mock.Mock()
-    monkeypatch.setattr("acondbs.github.call.requests", y)
-    response = {
-        'error': 'bad_verification_code',
-        'error_description': 'The code passed is incorrect or expired.',
-        'error_uri': 'https://docs.github.com/apps/managing-oauth-apps/troubleshooting-oauth-app-access-token-request-errors/#bad-verification-code'
-    }
-    r = mock.Mock()
-    r.json.return_value = response
-    y.post.return_value = r
-    yield y
-
 @pytest.fixture(autouse=True)
-def mock_get_user(monkeypatch):
+def mock_store_token_for_code(monkeypatch):
     y = mock.Mock()
-    monkeypatch.setattr("acondbs.schema.github.mutation.get_user", y)
+    monkeypatch.setattr("acondbs.schema.github.mutation.store_token_for_code", y)
     yield y
 
 # __________________________________________________________________||
@@ -72,31 +48,15 @@ params = [
             "name": "User One",
             "avatarUrl": "https://avatars0.githubusercontent.com/u/user1"
         },
-        id='existing-user'
-    ),
-    pytest.param(
-        [
-            [ADD_GIT_HUB_ADMIN_APP_TOKEN],
-            {'variables': {
-                'code': 'code_01234'
-            }},
-        ],
-        [[ALL_GITHUB_ADMIN_APP_TOKENS], {}],
-        {
-            "login": "new-user",
-            "name": "New User",
-            "avatarUrl": "https://avatars0.githubusercontent.com/u/new-user"
-        },
-        id='new-user'
+        id='one'
     ),
 ]
 
 @pytest.mark.parametrize('mutation, query, viewer', params)
-def test_schema_success(app, snapshot, mutation, query, viewer, mock_request_backup_db, mock_auth_requests_success, mock_get_user):
-    mock_get_user.return_value = viewer
+def test_schema_success(app, snapshot, mutation, query, viewer, mock_request_backup_db, mock_store_token_for_code):
     assert_mutation(app, snapshot, mutation, query,
                     mock_request_backup_db, success=True)
-    snapshot.assert_match(mock_auth_requests_success.post.call_args_list)
+    snapshot.assert_match(mock_store_token_for_code.call_args_list)
 
 # __________________________________________________________________||
 params = [
@@ -108,14 +68,15 @@ params = [
             }},
         ],
         [[ALL_GITHUB_ADMIN_APP_TOKENS], {}],
-        id='add'
+        id='one'
     ),
 ]
 
 @pytest.mark.parametrize('mutation, query', params)
-def test_schema_error(app, snapshot, mutation, query, mock_request_backup_db, mock_auth_requests_error):
+def test_schema_error(app, snapshot, mutation, query, mock_request_backup_db, mock_store_token_for_code):
+    mock_store_token_for_code.side_effect = Exception("error")
     assert_mutation(app, snapshot, mutation, query,
                     mock_request_backup_db, success=False)
-    snapshot.assert_match(mock_auth_requests_error.post.call_args_list)
+    snapshot.assert_match(mock_store_token_for_code.call_args_list)
 
 # __________________________________________________________________||
