df961dbe89064de1c82bd72ef95f7aa9ba191c05
Tai Sakuma <tai.sakuma@gmail.com>
2021-01-05 15:24:16 -0500

    fix indent

--- acondbs/schema/filter_.py
+++ acondbs/schema/filter_.py
@@ -11,33 +11,33 @@ import graphene
 from graphene_sqlalchemy_filter import FilterableConnectionField, FilterSet
 
 from ..models import (
-   Product as ProductModel,
-   ProductType as ProductTypeModel,
-   GitHubToken as GitHubTokenModel
+    Product as ProductModel,
+    ProductType as ProductTypeModel,
+    GitHubToken as GitHubTokenModel
 )
 
 ##__________________________________________________________________||
 class ProductFilter(FilterSet):
-   type_name = graphene.String()
+    type_name = graphene.String()
 
-   class Meta:
-       model = ProductModel
-       fields = {
-           'type_id': ['eq', ],
-       }
+    class Meta:
+        model = ProductModel
+        fields = {
+            'type_id': ['eq', ],
+        }
 
-   @staticmethod
-   def type_name_filter(info, query, value):
-       # "Filters that require join":
-       # https://github.com/art1415926535/graphene-sqlalchemy-filter/tree/1.10.2#filters-that-require-join
-       query = query.join(ProductTypeModel)
-       filter_ = ProductTypeModel.name == value
-       return query, filter_
+    @staticmethod
+    def type_name_filter(info, query, value):
+        # "Filters that require join":
+        # https://github.com/art1415926535/graphene-sqlalchemy-filter/tree/1.10.2#filters-that-require-join
+        query = query.join(ProductTypeModel)
+        filter_ = ProductTypeModel.name == value
+        return query, filter_
 
 class ProductTypeFilter(FilterSet):
-   class Meta:
-       model = ProductTypeModel
-       fields = { }
+    class Meta:
+        model = ProductTypeModel
+        fields = { }
 
 ##__________________________________________________________________||
 class GitHubTokenFilter(FilterSet):
