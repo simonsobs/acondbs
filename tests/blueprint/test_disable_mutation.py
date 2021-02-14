65f18c5011a6f05b843cbe6ce920faf00c9f6bd9
Tai Sakuma <tai.sakuma@gmail.com>
2021-02-13 21:53:51 -0500

    split test_bpgraphql.py into three files

--- tests/blueprint/test_disable_mutation.py
+++ tests/blueprint/test_disable_mutation.py
@@ -1,22 +1,13 @@
 from flask import json
 import pytest
 
-##__________________________________________________________________||
-def test_graphql(client):
-    response = client.get(
-        '/graphql',
-        query_string=dict(query='{ allProducts { edges { node { id name } } }}'))
-    assert 200 == response.status_code
-
-    un_jsonified = json.loads(response.data)
-    # print(json.dumps(un_jsonified, indent=2))
-
-##__________________________________________________________________||
 from acondbs import create_app
-from .constants import SAMPLE_DIR
 from pathlib import Path
 from acondbs.db.ops import define_tables
 
+from ..constants import SAMPLE_DIR
+
+##__________________________________________________________________||
 params = [
     [{'ACONDBS_SCHEME_MUTATION_DISABLE': True}, False],
     [{'ACONDBS_SCHEME_MUTATION_DISABLE': False}, True]
@@ -57,26 +48,5 @@ def test_disable_mutation(kwargs, mutation):
     result = json.loads(response.data)['data']
     assert result['Query']
     assert mutation == bool(result['Mutation'])
-
-##__________________________________________________________________||
-params = [
-    [{'ACONDBS_GRAPHIQL': True}, 200, '<!DOCTYPE html>'],
-    [{'ACONDBS_GRAPHIQL': False}, 400, 'errors'],
-]
-
-@pytest.mark.parametrize('kwargs, code, data', params)
-def test_disable_graphiql(kwargs, code, data, snapshot):
-    config_path = Path(SAMPLE_DIR, 'config.py')
-    kwargs.update(dict(
-        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:"
-    ))
-    app = create_app(config_path, **kwargs)
-    with app.app_context():
-        define_tables()
-    client = app.test_client()
-
-    response = client.get('/graphql', headers={"Accept": "text/html"})
-    assert code == response.status_code
-    assert data in response.data.decode("utf-8")
 
 ##__________________________________________________________________||
---
+++ tests/blueprint/test_graphql.py
@@ -0,0 +1,14 @@
+from flask import json
+import pytest
+
+##__________________________________________________________________||
+def test_graphql(client):
+    response = client.get(
+        '/graphql',
+        query_string=dict(query='{ allProducts { edges { node { id name } } }}'))
+    assert 200 == response.status_code
+
+    un_jsonified = json.loads(response.data)
+    # print(json.dumps(un_jsonified, indent=2))
+
+##__________________________________________________________________||
