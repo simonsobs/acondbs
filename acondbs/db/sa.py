eb06a4d9d26a5651ae990d518c7e08dda3fbba39
Tai Sakuma <tai.sakuma@gmail.com>
2021-02-13 21:06:43 -0500

    update docstring

--- acondbs/db/sa.py
+++ acondbs/db/sa.py
@@ -21,8 +21,8 @@ metadata = MetaData(naming_convention=convention)
 sa = SQLAlchemy(metadata=metadata)
 """the instance of SQLAlchemy from Flask-SQLAlchemy
 
-The instance is named "sa" here. However, it is more commonly named
-"db" in example code found online.
+While the instance is named "sa" here, it is more commonly named "db"
+in example code found online.
 
 - API: https://flask-sqlalchemy.palletsprojects.com/en/2.x/api/#flask_sqlalchemy.SQLAlchemy
 - The difference from the plain SQLAlchemy:
