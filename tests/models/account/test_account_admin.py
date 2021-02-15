705c357934931fe12bb328aee4bdc1b09cc8994d
Tai Sakuma <tai.sakuma@gmail.com>
2021-02-15 11:29:18 -0500

    add test for AccountAdmin

---
+++ tests/models/account/test_account_admin.py
@@ -0,0 +1,18 @@
+from acondbs.db.sa import sa
+from acondbs.models import AccountAdmin
+
+##__________________________________________________________________||
+def test_entry(app_empty):
+    app = app_empty
+
+    admin1 = AccountAdmin(git_hub_login="octocat")
+
+    with app.app_context():
+        sa.session.add(admin1)
+        sa.session.commit()
+
+    with app.app_context():
+        admin1 = AccountAdmin.query.filter_by(git_hub_login='octocat').one()
+        assert 'octocat' == admin1.git_hub_login
+
+##__________________________________________________________________||
