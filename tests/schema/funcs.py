2925c2c6bfce508d228b3cbb7fe744bd0625b03f
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-01 14:51:04 -0400

    format source

--- tests/schema/funcs.py
+++ tests/schema/funcs.py
@@ -2,6 +2,7 @@ from graphene.test import Client
 
 from acondbs.schema import schema as default_schema
 
+
 ##__________________________________________________________________||
 def assert_query(app, snapshot, query, error=False, schema=default_schema):
 
@@ -12,10 +13,10 @@ def assert_query(app, snapshot, query, error=False, schema=default_schema):
     # arguments to client.execute()
     args, kwargs = query
 
-    if 'context_value' not in kwargs:
+    if "context_value" not in kwargs:
         # provide an empty dict to prevent errors
         # in graphene_sqlalchemy_filter.
-        kwargs['context_value'] = {}
+        kwargs["context_value"] = {}
 
         # The detail is described in the commit message:
         #   https://github.com/simonsobs/acondbs/commit/0c65d5719129c4940eea0763ae7b9cc1bcddfe64
@@ -28,20 +29,29 @@ def assert_query(app, snapshot, query, error=False, schema=default_schema):
 
     # assert errors
     if error:
-        assert 'errors' in result
+        assert "errors" in result
         return
 
-    assert 'errors' not in result
+    assert "errors" not in result
 
     # snapshot test
     #   https://github.com/syrusakbary/snapshottest/
     snapshot.assert_match(result)
 
-def assert_mutation(app, snapshot, mutation, query, mock_request_backup_db,
-                    success=True, schema=default_schema):
+
+def assert_mutation(
+    app,
+    snapshot,
+    mutation,
+    query,
+    mock_request_backup_db,
+    success=True,
+    schema=default_schema,
+):
     assert_query(app, snapshot, mutation, error=not success, schema=schema)
     assert_query(app, snapshot, query, schema=schema)
     if success:
         mock_request_backup_db.assert_called()
+
 
 ##__________________________________________________________________||
