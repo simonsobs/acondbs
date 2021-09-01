cd8faf1634f6eaefc01140edcbc2e64ef4bd93e3
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-01 13:57:43 -0400

    format source

--- tests/blueprint/test_disable_graphiql.py
+++ tests/blueprint/test_disable_graphiql.py
@@ -8,23 +8,23 @@ from ..constants import SAMPLE_DIR
 
 ##__________________________________________________________________||
 params = [
-    [{'ACONDBS_GRAPHIQL': True}, 200, '<!DOCTYPE html>'],
-    [{'ACONDBS_GRAPHIQL': False}, 400, 'errors'],
+    [{"ACONDBS_GRAPHIQL": True}, 200, "<!DOCTYPE html>"],
+    [{"ACONDBS_GRAPHIQL": False}, 400, "errors"],
 ]
 
-@pytest.mark.parametrize('kwargs, code, data', params)
+
+@pytest.mark.parametrize("kwargs, code, data", params)
 def test_disable_graphiql(kwargs, code, data, snapshot):
-    config_path = Path(SAMPLE_DIR, 'config.py')
-    kwargs.update(dict(
-        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:"
-    ))
+    config_path = Path(SAMPLE_DIR, "config.py")
+    kwargs.update(dict(SQLALCHEMY_DATABASE_URI="sqlite:///:memory:"))
     app = create_app(config_path, **kwargs)
     with app.app_context():
         define_tables()
     client = app.test_client()
 
-    response = client.get('/graphql', headers={"Accept": "text/html"})
+    response = client.get("/graphql", headers={"Accept": "text/html"})
     assert code == response.status_code
     assert data in response.data.decode("utf-8")
+
 
 ##__________________________________________________________________||
