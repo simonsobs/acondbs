f779220eff8f0969c1ae808384f48a66a1d1ae58
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-03 16:13:06 -0400

    rewrite test_admin.py

--- tests/schema/auth/query/snapshots/snap_test_admin.py
+++ tests/schema/auth/query/snapshots/snap_test_admin.py
@@ -7,26 +7,42 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_success[dojocat] 1'] = {
+snapshots['test_scratch[dojocat] 1'] = {
     'data': {
         'isAdmin': False
     }
 }
 
-snapshots['test_success[no-token] 1'] = {
-    'data': {
-        'isAdmin': False
-    }
+snapshots['test_scratch[no-token] 1'] = {
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 3,
+                    'line': 1
+                }
+            ],
+            'message': 'Cannot query field "isAdmin" on type "QueryPublic".'
+        }
+    ]
 }
 
-snapshots['test_success[octocat] 1'] = {
+snapshots['test_scratch[octocat] 1'] = {
     'data': {
         'isAdmin': True
     }
 }
 
-snapshots['test_success[wrong-token] 1'] = {
-    'data': {
-        'isAdmin': False
-    }
+snapshots['test_scratch[wrong-token] 1'] = {
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 3,
+                    'line': 1
+                }
+            ],
+            'message': 'Cannot query field "isAdmin" on type "QueryPublic".'
+        }
+    ]
 }
