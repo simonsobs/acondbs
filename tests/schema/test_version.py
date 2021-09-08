d9e032dd25b338fa405ba85caa7a768288c9575c
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-08 16:21:23 -0400

    rewrite schema tests with asgi test client

--- tests/schema/test_version.py
+++ tests/schema/test_version.py
@@ -1,23 +1,32 @@
 import pytest
-from graphene.test import Client
+
+from async_asgi_testclient import TestClient
+from a2wsgi import WSGIMiddleware
 
 import acondbs
-from acondbs.schema import schema_admin
+
+QUERY = "{ version }"
+
 
 ##__________________________________________________________________||
-params = [
-    pytest.param(
-        '{ version }',
-        {'version': acondbs.__version__},
-        id='version'
-    ),
-]
+@pytest.mark.asyncio
+async def test_schema(app):
 
-@pytest.mark.parametrize('query, expected', params)
-def test_schema(app, query, expected):
-    with app.app_context():
-        client = Client(schema_admin)
-        result = client.execute(query, context_value={})
-        assert {'data': expected} == result
+    app = WSGIMiddleware(app)  # convert a wsgi app to an asgi app
+
+    headers = {
+        "Content-Type:": "application/json",
+        "Authorization": "Bearer token123",  # octocat
+    }
+
+    data = {"query": QUERY}
+    expected = {"data": {"version": acondbs.__version__}}
+
+    async with TestClient(app) as client:
+        resp = await client.post("/graphql", json=data, headers=headers)
+
+    assert resp.status_code == 200
+    assert resp.json() == expected
+
 
 ##__________________________________________________________________||
