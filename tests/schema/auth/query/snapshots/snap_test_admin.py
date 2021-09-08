99b2ad39d678b27da1761e624319d0c83b5b5fb1
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-08 17:18:40 -0400

    fix test errors

--- tests/schema/auth/query/snapshots/snap_test_admin.py
+++ tests/schema/auth/query/snapshots/snap_test_admin.py
@@ -13,36 +13,8 @@ snapshots['test_schema[dojocat] 1'] = {
     }
 }
 
-snapshots['test_schema[no-token] 1'] = {
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 3,
-                    'line': 1
-                }
-            ],
-            'message': 'Cannot query field "isAdmin" on type "QueryPublic".'
-        }
-    ]
-}
-
 snapshots['test_schema[octocat] 1'] = {
     'data': {
         'isAdmin': True
     }
-}
-
-snapshots['test_schema[wrong-token] 1'] = {
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 3,
-                    'line': 1
-                }
-            ],
-            'message': 'Cannot query field "isAdmin" on type "QueryPublic".'
-        }
-    ]
 }
--- tests/schema/auth/query/test_admin.py
+++ tests/schema/auth/query/test_admin.py
@@ -2,37 +2,41 @@ import pytest
 
 from ...funcs import assert_query
 
-QUERY = '{ isAdmin }'
+QUERY = "{ isAdmin }"
 
 ##__________________________________________________________________||
 params = [
     pytest.param(
         {"query": QUERY},
         {"Authorization": "Bearer 90b2ee5fed25506df04fd37343bb68d1803dd97f"},
+        False,
         id="octocat",
     ),
     pytest.param(
         {"query": QUERY},
         {"Authorization": "Bearer 0fb8c9e16d6f7c4961c4c49212bf197d79f14080"},
+        False,
         id="dojocat",
     ),
     pytest.param(
         {"query": QUERY},
         {"Authorization": "Bearer 1a2d18f270df3abacfb85c5413b668f97794b4ce"},
+        True,
         id="wrong-token",
     ),
     pytest.param(
         {"query": QUERY},
         {},
+        True,
         id="no-token",
     ),
 ]
 
 
-@pytest.mark.parametrize("data, headers", params)
+@pytest.mark.parametrize("data, headers, error", params)
 @pytest.mark.asyncio
-async def test_schema(app, snapshot, data, headers):
-    await assert_query(app, snapshot, data, headers)
+async def test_schema(app, snapshot, data, headers, error):
+    await assert_query(app, snapshot, data, headers, error)
 
 
 ##__________________________________________________________________||
