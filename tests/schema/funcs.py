fd46b3b0da3aba5c2d6cf0b2cd92ba4a225ee10e
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-09 16:58:40 -0400

    remove assert_query_graphene_client()

--- tests/schema/funcs.py
+++ tests/schema/funcs.py
@@ -1,9 +1,6 @@
-from graphene.test import Client
 from async_asgi_testclient import TestClient
-
 from a2wsgi import WSGIMiddleware
 
-from acondbs.schema import schema as default_schema
 
 HEADERS_DEFAULT = {"Content-Type:": "application/json"}
 
@@ -12,10 +9,6 @@ HEADERS_DEFAULT = {"Content-Type:": "application/json"}
 async def assert_query(app, snapshot, data, headers=None, error=False):
     if not headers:
         headers = {}
-    await assert_query_asgi_client(app, snapshot, data, headers, error=error)
-
-
-async def assert_query_asgi_client(app, snapshot, data, headers, error=False):
 
     app = WSGIMiddleware(app)  # convert a wsgi app to an asgi app
 
@@ -38,43 +31,6 @@ async def assert_query_asgi_client(app, snapshot, data, headers, error=False):
     # snapshot test
     #   https://github.com/syrusakbary/snapshottest/
     snapshot.assert_match(resp_json)
-
-
-def assert_query_graphene_client(
-    app, snapshot, query, error=False, schema=default_schema
-):
-
-    # create test client
-    # https://docs.graphene-python.org/en/latest/testing/
-    client = Client(schema)
-
-    # arguments to client.execute()
-    args, kwargs = query
-
-    if "context_value" not in kwargs:
-        # provide an empty dict to prevent errors
-        # in graphene_sqlalchemy_filter.
-        kwargs["context_value"] = {}
-
-        # The detail is described in the commit message:
-        #   https://github.com/simonsobs/acondbs/commit/0c65d5719129c4940eea0763ae7b9cc1bcddfe64
-        # A related issue in graphene:
-        #   https://github.com/graphql-python/graphene/issues/591
-
-    # execute the query
-    with app.app_context():
-        result = client.execute(*args, **kwargs)
-
-    # assert errors
-    if error:
-        assert "errors" in result
-        return
-
-    assert "errors" not in result
-
-    # snapshot test
-    #   https://github.com/syrusakbary/snapshottest/
-    snapshot.assert_match(result)
 
 
 async def assert_mutation(
