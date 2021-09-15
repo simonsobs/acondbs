9d11711d0d784609b9f79eee2737a3dcfcdc2377
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-15 14:00:50 -0400

    move GraphQL query strings

--- tests/schema/product/gql/mutations/__init__.py
+++ tests/schema/product/gql/mutations/__init__.py
@@ -1,3 +1,6 @@
 from .mutation_create_product import MUTATION_CREATE_PRODUCT  # noqa: F401
 from .mutation_delete_product import MUTATION_DELETE_PRODUCT  # noqa: F401
 from .mutation_update_product import MUTATION_UPDATE_PRODUCT  # noqa: F401
+from .mutation_create_product_relation_types import MUTATION_CREATE_PRODUCT_RELATION_TYPES  # noqa: F401
+from .mutation_update_product_relation_type import MUTATION_UPDATE_PRODUCT_RELATION_TYPE  # noqa: F401
+from .mutation_delete_product_relation_types import MUTATION_DELETE_PRODUCT_RELATION_TYPES  # noqa: F401
