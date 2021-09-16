5dcbc74d4383964e56c502c82eac7fa048a66ac9
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-16 15:42:38 -0400

    update tests

--- tests/models/product/attribute/conftest.py
+++ tests/models/product/attribute/conftest.py
@@ -18,24 +18,81 @@ def app(app_empty):
 
     y = app_empty
 
-    type1 = ProductType(name="type1")
+    # create product types
+    Map = ProductType(
+        type_id=1,
+        name="map",
+        order=2,
+        indef_article="a",
+        singular="map",
+        plural="maps",
+        icon="mdi-map",
+    )
+    Beam = ProductType(
+        type_id=2,
+        name="beam",
+        order=1,
+        indef_article="a",
+        singular="beam",
+        plural="beams",
+        icon="mdi-spotlight-beam",
+    )
 
-    product1 = Product(name="product1", type_=type1)
-
-    AttributeText(product=product1, name="note", value="This is a test")
+    # create products
+    map1 = Product(product_id=1, name="map1", type_=Map)
+    AttributeText(product=map1, name="name", value="map1")
     AttributeDate(
-        product=product1,
-        name="date_produced",
-        value=datetime.date(2019, 2, 23),
+        product=map1, name="date_produced", value=datetime.date(2020, 2, 1)
     )
     AttributeDateTime(
-        product=product1,
+        product=map1,
         name="time_posted",
-        value=datetime.datetime(2019, 2, 24, 9, 10, 25),
+        value=datetime.datetime(2020, 2, 1, 9, 10, 25),
+    )
+    map2 = Product(product_id=2, name="map2", type_=Map)
+    AttributeText(product=map2, name="name", value="map2")
+    AttributeDate(
+        product=map2, name="date_produced", value=datetime.date(2020, 2, 10)
+    )
+    AttributeDateTime(
+        product=map2,
+        name="time_posted",
+        value=datetime.datetime(2020, 2, 10, 13, 20, 2),
+    )
+    map3 = Product(product_id=3, name="map3", type_=Map)
+    AttributeText(product=map3, name="name", value="map3")
+    AttributeDate(
+        product=map3, name="date_produced", value=datetime.date(2020, 3, 19)
+    )
+    AttributeDateTime(
+        product=map3,
+        name="time_posted",
+        value=datetime.datetime(2020, 3, 20, 8, 45, 30),
+    )
+    beam1 = Product(product_id=4, name="beam1", type_=Beam)
+    AttributeText(product=beam1, name="name", value="beam1")
+    AttributeDate(
+        product=beam1, name="date_produced", value=datetime.date(2020, 2, 5)
+    )
+    AttributeDateTime(
+        product=beam1,
+        name="time_posted",
+        value=datetime.datetime(2020, 2, 5, 12, 3, 48),
+    )
+    beam2 = Product(product_id=5, name="beam2", type_=Beam)
+    AttributeText(product=beam2, name="name", value="beam2")
+    AttributeDate(
+        product=beam2, name="date_produced", value=datetime.date(2020, 3, 4)
+    )
+    AttributeDateTime(
+        product=beam2,
+        name="time_posted",
+        value=datetime.datetime(2020, 3, 4, 19, 22, 5),
     )
 
     with y.app_context():
-        sa.session.add(product1)
+        sa.session.add(Map)
+        sa.session.add(Beam)
         sa.session.commit()
 
     yield y
--- tests/models/product/attribute/test_attribute.py
+++ tests/models/product/attribute/test_attribute.py
@@ -1,22 +1,123 @@
 import datetime
+from sqlalchemy.orm import aliased
 
-from acondbs.models import Product
+from acondbs.db.sa import sa
+
+from acondbs.models import Product, ProductType, AttributeText, AttributeDate
 
 
 ##__________________________________________________________________||
-def test_one(app):
+def test_map1_attributes(app):
     with app.app_context():
-        product1 = Product.query.filter_by(name="product1").one_or_none()
-        assert product1 is not None
-        note = product1.attributes_text[0]
-        assert "note" == note.name
-        assert "This is a test" == note.value
-        date_produced = product1.attributes_date[0]
-        assert "date_produced" == date_produced.name
-        assert datetime.date(2019, 2, 23) == date_produced.value
-        time_posted = product1.attributes_date_time[0]
-        assert "time_posted" == time_posted.name
-        assert datetime.datetime(2019, 2, 24, 9, 10, 25) == time_posted.value
+        map1 = Product.query.filter_by(product_id=1).one()
+
+        assert len(map1.attributes_text) == 1
+        name = [a for a in map1.attributes_text if a.name == "name"][0]
+        assert name.value == "map1"
+
+        assert len(map1.attributes_date) == 1
+        date_produced = [
+            a for a in map1.attributes_date if a.name == "date_produced"
+        ][0]
+        assert date_produced.value == datetime.date(2020, 2, 1)
+
+        assert len(map1.attributes_date_time) == 1
+        time_posted = [
+            a for a in map1.attributes_date_time if a.name == "time_posted"
+        ][0]
+        assert time_posted.value == datetime.datetime(2020, 2, 1, 9, 10, 25)
+
+
+def test_filter(app):
+
+    with app.app_context():
+        expected = Product.query.filter_by(product_id=1).one()
+        actual = (
+            Product.query.join(AttributeText)
+            .filter_by(name="name", value="map1")
+            .one()
+        )
+        assert actual is expected
+
+
+def test_order(app):
+
+    with app.app_context():
+        map1 = Product.query.filter_by(product_id=1).one()
+        map2 = Product.query.filter_by(product_id=2).one()
+        map3 = Product.query.filter_by(product_id=3).one()
+
+        expected = [map3, map2, map1]
+
+        actual = (
+            Product.query.join(ProductType)
+            .filter_by(name="map")
+            .join(AttributeDate)
+            .filter_by(name="date_produced")
+            .order_by(AttributeDate.value.desc())
+            .all()
+        )
+
+        assert actual == expected
+
+
+##__________________________________________________________________||
+def test_order_nested(app_empty):
+    app = app_empty
+
+    Map = ProductType(type_id=1, name="map")
+    map1 = Product(product_id=1, name="map1", type_=Map)
+    map2 = Product(product_id=2, name="map2", type_=Map)
+    map3 = Product(product_id=3, name="map3", type_=Map)
+    map4 = Product(product_id=4, name="map4", type_=Map)
+    map5 = Product(product_id=5, name="map5", type_=Map)
+
+    AttributeText(product=map1, name="attr1", value="1")
+    AttributeText(product=map2, name="attr1", value="2")
+    AttributeText(product=map3, name="attr1", value="1")
+    AttributeText(product=map4, name="attr1", value="2")
+    AttributeText(product=map5, name="attr1", value="1")
+
+    AttributeText(product=map1, name="attr2", value="b")
+    AttributeText(product=map2, name="attr2", value="a")
+    AttributeText(product=map3, name="attr2", value="c")
+    AttributeText(product=map4, name="attr2", value="b")
+    AttributeText(product=map5, name="attr2", value="a")
+
+    with app.app_context():
+        sa.session.add(Map)
+        sa.session.commit()
+
+    with app.app_context():
+
+        map1 = Product.query.filter_by(product_id=1).one()
+        map2 = Product.query.filter_by(product_id=2).one()
+        map3 = Product.query.filter_by(product_id=3).one()
+        map4 = Product.query.filter_by(product_id=4).one()
+        map5 = Product.query.filter_by(product_id=5).one()
+
+        expected = [map2, map4, map5, map1, map3]
+
+        # ORM Alias: https://docs.sqlalchemy.org/en/14/tutorial/data_select.html#orm-entity-aliases
+        # refer to the same table multiple times
+        AliasedAttributeText1 = aliased(AttributeText)
+        AliasedAttributeText2 = aliased(AttributeText)
+
+        # sort descending order of attr1 then ascending order of attr2
+        query = (
+            Product.query.join(ProductType)
+            .filter_by(name="map")
+            .join(AliasedAttributeText1)
+            .filter_by(name="attr1")
+            .order_by(AliasedAttributeText1.value.desc())
+            .join(AliasedAttributeText2)
+            .filter_by(name="attr2")
+            .order_by(AliasedAttributeText2.value)
+        )
+
+        actual = query.all()
+
+        assert actual == expected
 
 
 ##__________________________________________________________________||
