57f860011895242a6ea5ad9290f5f33505c75cbd
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-16 16:36:40 -0400

    format source

--- tests/schema/product/gql/mutations/mutation_create_product_relation_types.py
+++ tests/schema/product/gql/mutations/mutation_create_product_relation_types.py
@@ -2,10 +2,15 @@ from ..fragments import FRAGMENT_PRODUCT_RELATION_TYPE
 
 MUTATION_CREATE_PRODUCT_RELATION_TYPES = '''
 mutation CreateProductRelationTypes(
-    $type: CreateProductRelationTypeInput!,
-    $reverse: CreateProductRelationTypeInput,
-    $selfReverse: Boolean) {
-  createProductRelationTypes(type: $type, reverse: $reverse, selfReverse: $selfReverse) {
+  $type: CreateProductRelationTypeInput!
+  $reverse: CreateProductRelationTypeInput
+  $selfReverse: Boolean
+) {
+  createProductRelationTypes(
+    type: $type
+    reverse: $reverse
+    selfReverse: $selfReverse
+  ) {
     ok
     productRelationType {
       ...fragmentProductRelationType
--- tests/schema/product/gql/mutations/mutation_update_product_relation_type.py
+++ tests/schema/product/gql/mutations/mutation_update_product_relation_type.py
@@ -2,7 +2,9 @@ from ..fragments import FRAGMENT_PRODUCT_RELATION_TYPE
 
 MUTATION_UPDATE_PRODUCT_RELATION_TYPE = '''
 mutation UpdateProductRelationType(
-    $typeId: Int!, $input: UpdateProductRelationTypeInput!) {
+  $typeId: Int!
+  $input: UpdateProductRelationTypeInput!
+) {
   updateProductRelationType(typeId: $typeId, input: $input) {
     ok
     productRelationType {
