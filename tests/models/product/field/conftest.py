826db4610675ef91c3647054e39cf782c2559dd5
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-22 14:15:40 -0400

    add tests

---
+++ tests/models/product/field/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+
+from acondbs.db.sa import sa
+from acondbs.models import ProductType, FieldType, Field, TypeFieldAssociation
+
+
+##__________________________________________________________________||
+@pytest.fixture
+def app(app_empty):
+
+    y = app_empty
+
+    field1 = Field(name="field1", type_=FieldType.UnicodeText)
+    field2 = Field(name="フィールド2", type_=FieldType.UnicodeText)
+
+    type1 = ProductType(  # noqa: F841
+        name="type1",
+        fields=[
+            TypeFieldAssociation(field=field1),
+            TypeFieldAssociation(field=field2),
+        ],
+    )
+
+    with y.app_context():
+        sa.session.add(field1)
+        sa.session.add(field2)
+        sa.session.commit()
+
+    yield y
+
+
+##__________________________________________________________________||
