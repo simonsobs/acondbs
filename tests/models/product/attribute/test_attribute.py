22301e60ed744b1b879f054c49c1ca0316d72112
Tai Sakuma <tai.sakuma@gmail.com>
2021-09-23 19:45:51 -0400

    update tests

--- tests/models/product/attribute/test_attribute.py
+++ tests/models/product/attribute/test_attribute.py
@@ -80,37 +80,28 @@ def test_commit(app_empty, field_type, AttributeClass, value):
 
 
 ##__________________________________________________________________||
-def test_map1_attributes(app):
-    with app.app_context():
-        map1 = Product.query.filter_by(product_id=1).one()
-
-        assert len(map1.attributes_unicode_text) == 1
-        name = [a for a in map1.attributes_unicode_text if a.name == "attr1"][0]
-        assert name.value == "value1"
-
-        assert len(map1.attributes_date) == 1
-        date_produced = [
-            a for a in map1.attributes_date if a.name == "date_produced"
-        ][0]
-        assert date_produced.value == datetime.date(2020, 2, 1)
-
-        assert len(map1.attributes_date_time) == 1
-        time_posted = [
-            a for a in map1.attributes_date_time if a.name == "time_posted"
-        ][0]
-        assert time_posted.value == datetime.datetime(2020, 2, 1, 9, 10, 25)
-
-
 def test_filter(app):
 
     with app.app_context():
         expected = Product.query.filter_by(product_id=1).one()
-        actual = (
-            Product.query.join(AttributeUnicodeText)
-            .filter_by(name="attr1", value="value1")
-            .one()
+        query = (
+            Product.query.join(AttributeUnicodeText).join(Field)
+            .filter(Field.name == "attr1", AttributeUnicodeText.value == "value1")
         )
+        actual = query.one()
         assert actual is expected
+
+        # another way
+        query = (
+            Product.query
+            .join(AttributeUnicodeText)
+            .filter_by(value="value1")
+            .join(Field)
+            .filter_by(name="attr1")
+        )
+        actual = query.one()
+        assert actual is expected
+
 
 def test_order(app):
 
@@ -121,14 +112,17 @@ def test_order(app):
 
         expected = [map3, map2, map1]
 
-        actual = (
+        query = (
             Product.query.join(ProductType)
             .filter_by(name="map")
-            .join(AttributeDate)
+            .join(AttributeDate).join(Field)
             .filter_by(name="date_produced")
             .order_by(AttributeDate.value.desc())
-            .all()
         )
+
+        print(query)
+
+        actual = query.all()
 
         assert actual == expected
 
@@ -137,24 +131,31 @@ def test_order(app):
 def test_order_nested(app_empty):
     app = app_empty
 
+    field_attr1 = Field(name="attr1", type_=FieldType.UnicodeText)
+    field_attr2 = Field(name="attr2", type_=FieldType.UnicodeText)
     Map = ProductType(type_id=1, name="map")
+    Map.fields = [
+        TypeFieldAssociation(field=field_attr1),
+        TypeFieldAssociation(field=field_attr2),
+    ]
+
     map1 = Product(product_id=1, name="map1", type_=Map)
     map2 = Product(product_id=2, name="map2", type_=Map)
     map3 = Product(product_id=3, name="map3", type_=Map)
     map4 = Product(product_id=4, name="map4", type_=Map)
     map5 = Product(product_id=5, name="map5", type_=Map)
 
-    AttributeUnicodeText(product=map1, name="attr1", value="1")
-    AttributeUnicodeText(product=map2, name="attr1", value="2")
-    AttributeUnicodeText(product=map3, name="attr1", value="1")
-    AttributeUnicodeText(product=map4, name="attr1", value="2")
-    AttributeUnicodeText(product=map5, name="attr1", value="1")
+    AttributeUnicodeText(product=map1, field=field_attr1, value="1")
+    AttributeUnicodeText(product=map2, field=field_attr1, value="2")
+    AttributeUnicodeText(product=map3, field=field_attr1, value="1")
+    AttributeUnicodeText(product=map4, field=field_attr1, value="2")
+    AttributeUnicodeText(product=map5, field=field_attr1, value="1")
 
-    AttributeUnicodeText(product=map1, name="attr2", value="b")
-    AttributeUnicodeText(product=map2, name="attr2", value="a")
-    AttributeUnicodeText(product=map3, name="attr2", value="c")
-    AttributeUnicodeText(product=map4, name="attr2", value="b")
-    AttributeUnicodeText(product=map5, name="attr2", value="a")
+    AttributeUnicodeText(product=map1, field=field_attr2, value="b")
+    AttributeUnicodeText(product=map2, field=field_attr2, value="a")
+    AttributeUnicodeText(product=map3, field=field_attr2, value="c")
+    AttributeUnicodeText(product=map4, field=field_attr2, value="b")
+    AttributeUnicodeText(product=map5, field=field_attr2, value="a")
 
     with app.app_context():
         sa.session.add(Map)
@@ -175,17 +176,24 @@ def test_order_nested(app_empty):
         AliasedAttributeUnicodeText1 = aliased(AttributeUnicodeText)
         AliasedAttributeUnicodeText2 = aliased(AttributeUnicodeText)
 
+        AliasedField1 = aliased(Field)
+        AliasedField2 = aliased(Field)
+
         # sort descending order of attr1 then ascending order of attr2
         query = (
             Product.query.join(ProductType)
             .filter_by(name="map")
             .join(AliasedAttributeUnicodeText1)
+            .join(AliasedField1)
             .filter_by(name="attr1")
             .order_by(AliasedAttributeUnicodeText1.value.desc())
-            .join(AliasedAttributeUnicodeText2)
+            .join(AliasedAttributeUnicodeText2, Product.attributes_unicode_text)
+            .join(AliasedField2)
             .filter_by(name="attr2")
             .order_by(AliasedAttributeUnicodeText2.value)
         )
+
+        print(query)
 
         actual = query.all()
 
@@ -203,7 +211,10 @@ def test_delte_orphan(app):
     with app.app_context():
         map1 = Product.query.filter_by(product_id=1).one_or_none()
         assert map1 is None
-        attr = AttributeUnicodeText.query.filter_by(name='attr1', value='value1').one_or_none()
+        attr = AttributeUnicodeText.query.filter_by(
+            name="attr1", value="value1"
+        ).one_or_none()
         assert attr is None
+
 
 ##__________________________________________________________________||
