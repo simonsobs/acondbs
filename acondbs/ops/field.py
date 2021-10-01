40ac016d03a82a1e95051f8e05b76cdd91708ad5
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-01 13:43:05 -0400

    update docstrings

--- acondbs/ops/field.py
+++ acondbs/ops/field.py
@@ -4,7 +4,23 @@ from ..db.sa import sa
 
 ##__________________________________________________________________||
 def create_field(name, type_, field_id=None):
-    """Create a field"""
+    """Instantiate SQLAlchemy ORM model "Field"
+
+    Parameters
+    ----------
+    name : str
+        The name of the new field
+    type_ : enum or int
+        The type of the new field, e.g., unicode text, int
+    field_id : int, optional
+        The ID of the new field
+
+    Returns
+    -------
+    object
+        The created model
+
+    """
     type_ = FieldType(type_)  # in case given by int
     model = Field(name=name, type_=type_, field_id=field_id)
     sa.session.add(model)
@@ -14,7 +30,20 @@ def create_field(name, type_, field_id=None):
 def update_field(field_id, name):
     """Update a field
 
-    Only name can be changed
+    Only name can be changed.
+
+    Parameters
+    ----------
+    field_id : int
+        The ID of the field to be updated
+    name : str
+        The new name
+
+    Returns
+    -------
+    object
+        The model
+
     """
     model = Field.query.filter_by(field_id=field_id).one()
     model.name = name
@@ -22,7 +51,18 @@ def update_field(field_id, name):
 
 
 def delete_field(field_id):
-    """Delete a field"""
+    """Delete a field
+
+    Parameters
+    ----------
+    field_id : int
+        The ID of the field to be deleted
+
+    Returns
+    -------
+    None
+
+    """
     model = Field.query.filter_by(field_id=field_id).one()
     sa.session.delete(model)
 
