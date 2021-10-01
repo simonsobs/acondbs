fb2558e0704dc4d3dde358faac260520e127e5ef
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-01 16:22:24 -0400

    update a fixture

--- tests/schema/product/mutation/product_type/conftest.py
+++ tests/schema/product/mutation/product_type/conftest.py
@@ -10,6 +10,7 @@ from acondbs.models import (
     Field,
     TypeFieldAssociation,
 )
+from acondbs import ops
 
 
 ##__________________________________________________________________||
@@ -18,46 +19,61 @@ def app(app_users):
 
     y = app_users
 
-    # create fields
-    field_contact = Field(name="contact", type_=FieldType.UnicodeText)
-    field_produced_by = Field(name="produced_by", type_=FieldType.UnicodeText)
-    field_date_produced = Field(name="date_produced", type_=FieldType.Date)
-    fields = [field_contact, field_produced_by, field_date_produced]
-
-    # create product types
-    Map = ProductType(
-        type_id=1,
-        name="map",
-        order=2,
-        indef_article="a",
-        singular="map",
-        plural="maps",
-        icon="mdi-map",
-        fields=[TypeFieldAssociation(field=f) for f in fields],
-    )
-    Beam = ProductType(
-        type_id=2,
-        name="beam",
-        order=1,
-        indef_article="a",
-        singular="beam",
-        plural="beams",
-        icon="mdi-spotlight-beam",
-        fields=[TypeFieldAssociation(field=f) for f in fields],
-    )
-
-    # create products
-    map1 = Product(  # noqa: F841
-        name="map1", date_produced=datetime.date(2020, 2, 1), type_=Map
-    )
-    map2 = Product(  # noqa: F841
-        name="map2", date_produced=datetime.date(2020, 5, 10), type_=Map
-    )
-    map3 = Product(  # noqa: F841
-        name="map3", date_produced=datetime.date(2020, 3, 3), type_=Map
-    )
-
     with y.app_context():
+
+        # fmt: off
+        field_contact = ops.create_field(field_id=1, name="contact", type_=FieldType.UnicodeText)
+        field_produced_by = ops.create_field(field_id=2, name="produced_by", type_=FieldType.UnicodeText)
+        field_date_produced = ops.create_field(field_id=3, name="date_produced", type_=FieldType.Date)
+        # fmt: on
+
+        fields = [field_contact, field_produced_by, field_date_produced]
+
+        # create product types
+        # Map = ProductType(
+        #     type_id=1,
+        #     name="map",
+        #     order=2,
+        #     indef_article="a",
+        #     singular="map",
+        #     plural="maps",
+        #     icon="mdi-map",
+        #     fields=[TypeFieldAssociation(field=f) for f in fields],
+        # )
+        Map = ops.create_product_type(
+            dict(
+                type_id=1,
+                name="map",
+                order=2,
+                indef_article="a",
+                singular="map",
+                plural="maps",
+                icon="mdi-map",
+            )
+        )
+        Map.fields = [TypeFieldAssociation(field=f) for f in fields]
+        Beam = ProductType(
+            type_id=2,
+            name="beam",
+            order=1,
+            indef_article="a",
+            singular="beam",
+            plural="beams",
+            icon="mdi-spotlight-beam",
+            fields=[TypeFieldAssociation(field=f) for f in fields],
+        )
+
+        # create products
+        map1 = Product(  # noqa: F841
+            name="map1", date_produced=datetime.date(2020, 2, 1), type_=Map
+        )
+        map2 = Product(  # noqa: F841
+            name="map2", date_produced=datetime.date(2020, 5, 10), type_=Map
+        )
+        map3 = Product(  # noqa: F841
+            name="map3", date_produced=datetime.date(2020, 3, 3), type_=Map
+        )
+
         sa.session.add(Map)
         sa.session.add(Beam)
         sa.session.commit()
