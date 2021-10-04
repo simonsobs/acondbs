60b7864c9453c66588f064e45b581761726e17c1
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-04 15:35:39 -0400

    clean code

--- acondbs/ops/product_type.py
+++ acondbs/ops/product_type.py
@@ -18,24 +18,37 @@ def create_product_type(**kwargs):
 
 def update_product_type(type_id, **kwargs):
     """Update a product type"""
+
     model = ProductType.query.filter_by(type_id=type_id).one()
     field_ids = kwargs.pop("field_ids", None)
+
     for k, v in kwargs.items():
         setattr(model, k, v)
+
     if field_ids:
+        new_field_ids = set(field_ids)
+        old_field_ids = {f.field_id for f in model.fields}
+
+        unchanged_field_ids = new_field_ids & old_field_ids
+        added_field_ids = new_field_ids - unchanged_field_ids
+        removed_field_ids = old_field_ids - unchanged_field_ids
+
+        field_dict = {f.field_id: f for f in model.fields}
+        #  {field_id: TypeFieldAssociation}
+
         with sa.session.no_autoflush:
-            old_field_dict = {f.field_id: f for f in model.fields}
-            new_fields = []
-            field_ids = sorted(set(field_ids))
-            for field_id in field_ids:
-                field = old_field_dict.pop(field_id, None)
-                if not field:
-                    field = Field.query.filter_by(field_id=field_id).one()
-                    field = TypeFieldAssociation(field=field)
-                new_fields.append(field)
-            for field in old_field_dict.values():
+
+            for field_id in removed_field_ids:
+                field = field_dict.pop(field_id)
                 sa.session.delete(field)
-            model.fields = new_fields
+
+            for field_id in added_field_ids:
+                field_ = Field.query.filter_by(field_id=field_id).one()
+                field = TypeFieldAssociation(field=field_)
+                field_dict[field_id] = field
+
+        model.fields = [field_dict[i] for i in sorted(new_field_ids)]
+
     return model
 
 
