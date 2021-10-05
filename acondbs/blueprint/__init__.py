9f78f1c1260b0904541a2ac5a093c642d1a2b6bb
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-05 12:29:52 -0400

    format source

--- acondbs/blueprint/__init__.py
+++ acondbs/blueprint/__init__.py
@@ -6,34 +6,44 @@ from .graphql_ide import GRAPHIQL_NEWER, GRAPHQL_PLAYGROUND
 
 ##__________________________________________________________________||
 from flask import request
+
+
 class GraphQLView(GraphQLView):
     def _format_request_to_str(self):
         import json
         import textwrap
+
         h = request.headers
         h = str(h)
         if request.data:
             data_dict = json.loads(request.data)
         else:
             data_dict = {}
-        m = '\n'.join([
-            textwrap.dedent('''
+        m = "\n".join(
+            [
+                textwrap.dedent(
+                    """
             {}:
             {}
-            ''').lstrip().format(k, textwrap.indent(str(v), '    ').rstrip()) for k, v in data_dict.items()]
+            """
+                )
+                .lstrip()
+                .format(k, textwrap.indent(str(v), " " * 4).rstrip())
+                for k, v in data_dict.items()
+            ]
         )
-        msg = textwrap.dedent('''
+        msg = textwrap.dedent(
+            """
         - received header
         {h}
         - received query
         {q}
         --- end ---
-        ''').format(
-            h=textwrap.indent(h, '    '),
-            q=textwrap.indent(m, '    ')
-        )
+        """
+        ).format(h=textwrap.indent(h, " " * 4), q=textwrap.indent(m, " " * 4))
         # print(msg)
         return msg
+
     def dispatch_request(self):
 
         res = super().dispatch_request()
@@ -48,14 +58,16 @@ class GraphQLView(GraphQLView):
                 print(msg)
                 print(res.status)
                 print(res.response)
-        except BaseException as error:
+        except BaseException as error:  # noqa: F841
             import traceback
+
             traceback.print_exc()
 
         return res
 
+
 class GraphQLViewW(GraphQLView):
-    '''A wrapper of GraphQLView.
+    """A wrapper of GraphQLView.
 
     Used to determine arguments to GraphQLView.as_view() for each view
     based on on the configuration and request.
@@ -75,21 +87,27 @@ class GraphQLViewW(GraphQLView):
     The arguments are determined in In __init__() and given to the
     base class GraphQLView.
 
-    '''
+    """
+
     def __init__(self, **kwargs):
-        kwargs.update({
-            'schema': _select_schema(),
-            'graphiql': current_app.config.get('ACONDBS_GRAPHIQL', False),
-            'graphiql_template': _select_graphiql_template()
-        })
+        kwargs.update(
+            {
+                "schema": _select_schema(),
+                "graphiql": current_app.config.get("ACONDBS_GRAPHIQL", False),
+                "graphiql_template": _select_graphiql_template(),
+            }
+        )
         super().__init__(**kwargs)
 
+
 ##__________________________________________________________________||
-bp = Blueprint('graphql', __name__)
-bp.add_url_rule('/graphql', view_func=GraphQLViewW.as_view('graphql'))
+bp = Blueprint("graphql", __name__)
+bp.add_url_rule("/graphql", view_func=GraphQLViewW.as_view("graphql"))
+
 
 def init_app(app):
     app.register_blueprint(bp)
+
 
 ##__________________________________________________________________||
 def _select_schema():
@@ -100,13 +118,15 @@ def _select_schema():
     else:
         return schema.schema_public
 
+
 def _select_graphiql_template():
-    template_no = current_app.config.get('ACONDBS_GRAPHIQL_TEMPLATE_NO', None)
+    template_no = current_app.config.get("ACONDBS_GRAPHIQL_TEMPLATE_NO", None)
     if template_no == 1:
         return GRAPHIQL_NEWER
     elif template_no == 2:
         return GRAPHQL_PLAYGROUND
     else:
         return None
+
 
 ##__________________________________________________________________||
