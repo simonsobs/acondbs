f6e282192f269b76696937df5dbbb92ce1762e09
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-07 12:22:11 -0400

    split a module into two

--- tests/db/ops/test_import_csv_encrypted.py
+++ tests/db/ops/test_import_csv_encrypted.py
@@ -4,8 +4,8 @@ import pytest
 from acondbs import create_app
 from acondbs.db.ops import define_tables
 from acondbs.db.ops import import_tables_from_csv_files
-from acondbs.db.ops import export_db_to_dict_of_dict_list
-from acondbs.db.ops import export_db_to_csv_files
+
+from acondbs.models import GitHubToken
 
 from ...constants import SAMPLE_DIR
 
@@ -24,23 +24,28 @@ def app():
 
 
 ##__________________________________________________________________||
-def test_non_empty(app, snapshot):
+def test_encrypted_field(app):
     csvdir = Path(SAMPLE_DIR, "csv")
     with app.app_context():
         import_tables_from_csv_files(csvdir)
-    with app.app_context():
-        snapshot.assert_match(export_db_to_dict_of_dict_list())
+        assert "token123" == GitHubToken.query.one().token
 
 
-##__________________________________________________________________||
-def test_empty(app, tmpdir_factory):
-    csvdir = str(tmpdir_factory.mktemp("csv"))
-
-    with app.app_context():
-        export_db_to_csv_files(csvdir)
+def test_how_to_encrypt_and_decrypt(app):
+    unencrypted = "token123"
+    encrypted = "aKjGknYDHY39Z2xAaN7+sQ=="
 
     with app.app_context():
-        import_tables_from_csv_files(csvdir)
+        secret_key = app.config["SECRET_KEY"]  # 'secret_key_test_123'
+
+    from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
+
+    engine = AesEngine()
+    engine._update_key(secret_key)
+    engine._set_padding_mechanism(None)
+
+    assert engine.encrypt(unencrypted) == encrypted
+    assert engine.decrypt(encrypted) == unencrypted
 
 
 ##__________________________________________________________________||
