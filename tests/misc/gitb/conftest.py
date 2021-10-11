d45d38d727e43c6d7e2017b5cba3fc6ae393cb34
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-11 11:07:13 -0400

    format source

--- tests/misc/gitb/conftest.py
+++ tests/misc/gitb/conftest.py
@@ -3,6 +3,7 @@ import git
 
 import pytest
 
+
 ##__________________________________________________________________||
 @pytest.fixture()
 def folder(tmpdir_factory):
@@ -15,12 +16,13 @@ def folder(tmpdir_factory):
        123
 
     """
-    folder = Path(tmpdir_factory.mktemp('git'))
-    file1 = folder.joinpath('f.txt')
-    file2 = folder.joinpath('g.txt')
-    file1.write_text('abc\n')
-    file2.write_text('123\n')
+    folder = Path(tmpdir_factory.mktemp("git"))
+    file1 = folder.joinpath("f.txt")
+    file2 = folder.joinpath("g.txt")
+    file1.write_text("abc\n")
+    file2.write_text("123\n")
     yield folder
+
 
 ##__________________________________________________________________||
 @pytest.fixture()
@@ -34,48 +36,54 @@ def repo(folder):
     """
     repo = git.Repo.init(folder)
     repo.git.add(A=True)
-    repo.index.commit('initial commit')
+    repo.index.commit("initial commit")
     yield repo
+
 
 ##__________________________________________________________________||
 @pytest.fixture()
 def bare_repo(repo, tmpdir_factory):
-    """a bare repo, a clone of the repo
-    """
-    folder = Path(tmpdir_factory.mktemp('git'))
+    """a bare repo, a clone of the repo"""
+    folder = Path(tmpdir_factory.mktemp("git"))
     y = git.Repo.clone_from(repo.working_tree_dir, folder, bare=True)
     yield y
 
+
 @pytest.fixture()
 def github_repo_url(bare_repo, tmpdir_factory):
-    """a factory fixture
-    """
-    url = 'git@github.com:TaiSakuma/fuzzy-garbanzo.git'
+    """a factory fixture"""
+    url = "git@github.com:TaiSakuma/fuzzy-garbanzo.git"
+
     def _f():
         """create a clone of the bare repo and push force
-           the content to the github repo
+        the content to the github repo
 
         """
-        folder = Path(tmpdir_factory.mktemp('git'))
+        folder = Path(tmpdir_factory.mktemp("git"))
         clone = git.Repo.clone_from(bare_repo.git_dir, folder)
         branch_name = clone.active_branch.name
-        remote = clone.create_remote('github', url=url)
-        remote.push(refspec=f'{branch_name}:{branch_name}', force=True)
+        remote = clone.create_remote("github", url=url)
+        remote.push(refspec=f"{branch_name}:{branch_name}", force=True)
         return url
+
     yield _f
 
-@pytest.fixture(params=[
-    'local_folder',
-    # 'github_repo'
-])
+
+@pytest.fixture(
+    params=[
+        "local_folder",
+        # 'github_repo'
+    ]
+)
 def remote_url(request, bare_repo, github_repo_url):
     p = request.param
-    if p == 'local_folder':
+    if p == "local_folder":
         y = bare_repo.git_dir
-    elif p == 'github_repo':
+    elif p == "github_repo":
         y = github_repo_url()
     else:
-        raise ValueError(f'unknown param: {p}')
+        raise ValueError(f"unknown param: {p}")
     yield y
+
 
 ##__________________________________________________________________||
