436d28cd81e11115edff63ed5beb0abef2717208
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-15 17:00:21 -0400

    update tests

--- tests/ops/test_product.py
+++ tests/ops/test_product.py
@@ -1,10 +1,11 @@
+import datetime
 import pytest
 
 # from sqlalchemy import exc
 
 from acondbs import ops
 from acondbs.ops.product import _normalize_paths
-from acondbs.models import Product, GitHubUser
+from acondbs.models import Product, FieldType, GitHubUser
 
 
 ##__________________________________________________________________||
@@ -31,6 +32,19 @@ def test_normalize_paths(paths, ret):
 params = [
     pytest.param(None, id="none"),
     pytest.param({}, id="empty"),
+    pytest.param({1: "An attribute"}, id="one"),
+    pytest.param({1: "An attribute", 3: False}, id="two"),
+    pytest.param(
+        {
+            1: "An attribute",
+            3: False,
+            4: True,
+            5: 512,
+            6: 2.34,
+            8: datetime.datetime(2021, 10, 15, 16, 55, 21),
+        },
+        id="all",
+    ),
 ]
 
 
@@ -45,7 +59,7 @@ params = [
     pytest.param([{"type_id": 1, "product_id": 2}], id="one"),
     pytest.param(
         [{"type_id": 2, "product_id": 1}, {"type_id": 1, "product_id": 3}],
-        id="one",
+        id="multiple",
     ),
 ]
 
@@ -128,6 +142,31 @@ def _test_create(
             assert actual == expected
         else:
             assert model.relations == []
+
+        if attributes is None:
+            attributes = {}
+        expected_field_ids = [f.field_id for f in model.type_.fields]
+        expected = {i: attributes.get(i) for i in expected_field_ids}
+        actual = _extract_attributes(model)
+        actual_field_ids = list(actual.keys())
+        assert actual_field_ids == expected_field_ids
+        assert actual == expected
+
+
+##__________________________________________________________________||
+def _extract_attributes(model):
+    attr_names = [
+        a.attribute_class.backref_column
+        for a in FieldType.__members__.values()
+    ]
+    # e.g., 'attributes_unicode_text', 'attributes_boolean'
+
+    attrs = [e for attr in attr_names for e in getattr(model, attr)]
+    # e.g., AttributeUnicodeText
+
+    ret = {a.field_id: a.value for a in attrs}
+
+    return ret
 
 
 ##__________________________________________________________________||
