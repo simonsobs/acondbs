5fb458208c4b097cf295a41618a97889fd42df7b
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-17 16:05:56 -0400

    rewrite with ops

--- tests/schema/product/mutation/product_type/conftest.py
+++ tests/schema/product/mutation/product_type/conftest.py
@@ -2,11 +2,7 @@ import pytest
 
 import datetime
 
-from acondbs.db.sa import sa
-from acondbs.models import (
-    Product,
-    FieldType,
-)
+from acondbs.models import FieldType
 from acondbs import ops
 
 
@@ -17,18 +13,38 @@ def app(app_users):
     y = app_users
 
     with y.app_context():
-        # fmt: off
-        ops.create_field(field_id=1, name="contact", type_=FieldType.UnicodeText)
-        ops.create_field(field_id=2, name="produced_by", type_=FieldType.UnicodeText)
-        ops.create_field(field_id=3, name="date_produced", type_=FieldType.Date)
-        ops.create_field(field_id=4, name="field_four", type_=FieldType.UnicodeText)
-        ops.create_field(field_id=5, name="field_five", type_=FieldType.UnicodeText)
-        # fmt: on
+
+        ops.create_field(
+            field_id=1,
+            name="contact",
+            type_=FieldType.UnicodeText,
+        )
+        ops.create_field(
+            field_id=2,
+            name="produced_by",
+            type_=FieldType.UnicodeText,
+        )
+        ops.create_field(
+            field_id=3,
+            name="date_produced",
+            type_=FieldType.Date,
+        )
+        ops.create_field(
+            field_id=4,
+            name="field_four",
+            type_=FieldType.UnicodeText,
+        )
+        ops.create_field(
+            field_id=5,
+            name="field_five",
+            type_=FieldType.UnicodeText,
+        )
+
         ops.commit()
 
     with y.app_context():
 
-        Map = ops.create_product_type(
+        ops.create_product_type(
             type_id=1,
             name="map",
             order=2,
@@ -38,7 +54,7 @@ def app(app_users):
             icon="mdi-map",
             field_ids=[1, 2, 3],
         )
-        Beam = ops.create_product_type(
+        ops.create_product_type(
             type_id=2,
             name="beam",
             order=1,
@@ -48,21 +64,26 @@ def app(app_users):
             icon="mdi-spotlight-beam",
             field_ids=[1, 2, 3],
         )
+        ops.commit()
 
-        # create products
-        map1 = Product(  # noqa: F841
-            name="map1", date_produced=datetime.date(2020, 2, 1), type_=Map
+    with y.app_context():
+        ops.create_product(
+            type_id=1,
+            name="map1",
+            date_produced=datetime.date(2020, 2, 1),
         )
-        map2 = Product(  # noqa: F841
-            name="map2", date_produced=datetime.date(2020, 5, 10), type_=Map
+        ops.create_product(
+            type_id=1,
+            name="map2",
+            date_produced=datetime.date(2020, 2, 10),
         )
-        map3 = Product(  # noqa: F841
-            name="map3", date_produced=datetime.date(2020, 3, 3), type_=Map
+        ops.create_product(
+            type_id=1,
+            name="map3",
+            date_produced=datetime.date(2020, 3, 3),
         )
+        ops.commit()
 
-        sa.session.add(Map)
-        sa.session.add(Beam)
-        sa.session.commit()
     yield y
 
 
