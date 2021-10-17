5d539ef7b54bee5a4f8be4df415463b011cf98d5
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-17 11:25:09 -0400

    test if request_backup_db() is only called once

--- tests/schema/funcs.py
+++ tests/schema/funcs.py
@@ -48,7 +48,7 @@ async def assert_mutation(
     )
     await assert_query(app, snapshot, data_query, headers_query)
     if success:
-        mock_request_backup_db.assert_called()
+        mock_request_backup_db.assert_called_once()
 
 
 ##__________________________________________________________________||
