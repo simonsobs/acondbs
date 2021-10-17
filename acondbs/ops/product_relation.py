4d1f1f60abc94e41ba66eca669583934ecc5d877
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-17 13:52:12 -0400

    add kwargs to create_product_relation()

--- acondbs/ops/product_relation.py
+++ acondbs/ops/product_relation.py
@@ -7,11 +7,11 @@ from ..models import (
 from ..db.sa import sa
 
 
-def create_product_relation(type_id, self_product_id, other_product_id):
+def create_product_relation(type_id, self_product_id, other_product_id, **kwargs):
     type_ = ProductRelationType.query.filter_by(type_id=type_id).one()
     self_ = Product.query.filter_by(product_id=self_product_id).one()
     other = Product.query.filter_by(product_id=other_product_id).one()
-    model = ProductRelation(type_=type_, self_=self_, other=other)
+    model = ProductRelation(type_=type_, self_=self_, other=other, **kwargs)
     sa.session.add(model)
     return model
 
