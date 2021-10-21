2f2d9ead1d5a1e21c0fa49c79e037c71f22b3f07
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-21 17:21:37 -0400

    clean code

--- tests/blueprint/test_disable_graphiql.py
+++ tests/blueprint/test_disable_graphiql.py
@@ -1,25 +1,26 @@
 import pytest
 
 from acondbs import create_app
-from pathlib import Path
 from acondbs.db.ops import define_tables
 
-from ..constants import SAMPLE_DIR
 
 ##__________________________________________________________________||
 params = [
-    [{"ACONDBS_GRAPHIQL": True}, 200, "<!DOCTYPE html>"],
-    [{"ACONDBS_GRAPHIQL": False}, 400, "errors"],
+    [True, 200, "<!DOCTYPE html>"],
+    [False, 400, "errors"],
 ]
 
 
-@pytest.mark.parametrize("kwargs, code, data", params)
-def test_disable_graphiql(kwargs, code, data, snapshot):
-    config_path = Path(SAMPLE_DIR, "config.py")
-    kwargs.update(dict(SQLALCHEMY_DATABASE_URI="sqlite:///:memory:"))
-    app = create_app(config_path, **kwargs)
+@pytest.mark.parametrize("graphiql, code, data", params)
+def test_disable_graphiql(graphiql, code, data):
+    app = create_app(
+        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
+        ACONDBS_GRAPHIQL=graphiql,
+        ACONDBS_GRAPHIQL_TEMPLATE_NO=2,
+    )
     with app.app_context():
         define_tables()
+
     client = app.test_client()
 
     response = client.get("/graphql", headers={"Accept": "text/html"})
