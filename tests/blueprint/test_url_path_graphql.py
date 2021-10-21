f40de32a9a0a79de9d51dfd3e60fd1e433be4069
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-21 16:36:29 -0400

    format source

--- tests/blueprint/test_url_path_graphql.py
+++ tests/blueprint/test_url_path_graphql.py
@@ -1,6 +1,4 @@
 from flask import json
-import pytest
-
 
 QUERY = """{
   webConfig {
@@ -12,15 +10,14 @@ QUERY = """{
 
 ##__________________________________________________________________||
 def test_graphql(client, snapshot):
-    path = '/graphql'
+    path = "/graphql"
 
-    response = client.get(
-        path,
-        query_string=dict(query=QUERY))
+    response = client.get(path, query_string=dict(query=QUERY))
     assert 200 == response.status_code
 
     un_jsonified = json.loads(response.data)
     snapshot.assert_match(un_jsonified)
     # print(json.dumps(un_jsonified, indent=2))
+
 
 ##__________________________________________________________________||
