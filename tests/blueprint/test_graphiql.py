c78a818a2c74db831c4886e645f4f721f2f5242a
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-21 17:23:01 -0400

    test merge files

--- tests/blueprint/test_graphiql.py
+++ tests/blueprint/test_graphiql.py
@@ -12,7 +12,7 @@ params = [
 
 
 @pytest.mark.parametrize("graphiql, code, data", params)
-def test_disable_graphiql(graphiql, code, data):
+def test_disable(graphiql, code, data):
     app = create_app(
         SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
         ACONDBS_GRAPHIQL=graphiql,
@@ -25,6 +25,31 @@ def test_disable_graphiql(graphiql, code, data):
 
     response = client.get("/graphql", headers={"Accept": "text/html"})
     assert code == response.status_code
+    assert data in response.data.decode("utf-8")
+
+
+##__________________________________________________________________||
+params = [
+    [None, "//cdn.jsdelivr.net/npm/graphiql@0.11.11/graphiql.min.js"],
+    [1, "https://unpkg.com/graphiql/graphiql.min.js"],
+    [2, "graphql-playground"],
+]
+
+
+@pytest.mark.parametrize("number, data", params)
+def test_template(number, data):
+    app = create_app(
+        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
+        ACONDBS_GRAPHIQL=True,
+        ACONDBS_GRAPHIQL_TEMPLATE_NO=number,
+    )
+    with app.app_context():
+        define_tables()
+
+    client = app.test_client()
+
+    response = client.get("/graphql", headers={"Accept": "text/html"})
+    assert 200 == response.status_code
     assert data in response.data.decode("utf-8")
 
 
