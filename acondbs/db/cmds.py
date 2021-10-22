b8a46714566840147646850806f19e8512c1d1cd
Tai Sakuma <tai.sakuma@gmail.com>
2021-10-22 18:38:37 -0400

    exclude the table logging from backup by default

--- acondbs/db/cmds.py
+++ acondbs/db/cmds.py
@@ -52,7 +52,7 @@ def export_csv_command(csvdir):
 
 
 ##__________________________________________________________________||
-DEFAULT_EXCLUDES = ("github_tokens", "account_admins")
+DEFAULT_EXCLUDES = ("github_tokens", "account_admins", "logging")
 
 
 @click.command("backup-db")
