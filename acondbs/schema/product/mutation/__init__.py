97815cd8b84731cca061019ad4e817efb00b37dd
Tai Sakuma <tai.sakuma@gmail.com>
2023-06-26 11:48:05 -0400

    update types

--- acondbs/schema/product/mutation/__init__.py
+++ acondbs/schema/product/mutation/__init__.py
@@ -1,23 +1,36 @@
-from .field import CreateField, DeleteField, UpdateField  # noqa: F401
-from .product import (  # noqa: F401
-    ConvertProductType,
-    CreateProduct,
-    DeleteProduct,
-    UpdateProduct,
-)
-from .product_file_path import (  # noqa: F401
+__all__ = [
+    'CreateField',
+    'DeleteField',
+    'UpdateField',
+    'ConvertProductType',
+    'CreateProduct',
+    'DeleteProduct',
+    'UpdateProduct',
+    'CreateProductFilePath',
+    'DeleteProductFilePath',
+    'UpdateProductFilePath',
+    'CreateProductRelation',
+    'DeleteProductRelation',
+    'CreateProductRelationTypes',
+    'DeleteProductRelationTypes',
+    'UpdateProductRelationType',
+    'CreateProductType',
+    'DeleteProductType',
+    'UpdateProductType',
+]
+
+
+from .field import CreateField, DeleteField, UpdateField
+from .product import ConvertProductType, CreateProduct, DeleteProduct, UpdateProduct
+from .product_file_path import (
     CreateProductFilePath,
     DeleteProductFilePath,
     UpdateProductFilePath,
 )
-from .product_relation import CreateProductRelation, DeleteProductRelation  # noqa: F401
-from .product_relation_type import (  # noqa: F401
+from .product_relation import CreateProductRelation, DeleteProductRelation
+from .product_relation_type import (
     CreateProductRelationTypes,
     DeleteProductRelationTypes,
     UpdateProductRelationType,
 )
-from .product_type import (  # noqa: F401
-    CreateProductType,
-    DeleteProductType,
-    UpdateProductType,
-)
+from .product_type import CreateProductType, DeleteProductType, UpdateProductType
