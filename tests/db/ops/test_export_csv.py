7d7882631e5b5b0ba847e5eb973ab614b29180ed
Tai Sakuma <tai.sakuma@gmail.com>
2023-06-27 09:09:09 -0400

    clean code

--- tests/db/ops/test_export_csv.py
+++ tests/db/ops/test_export_csv.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from flask import Flask
 
 from acondbs import create_app
 from acondbs.db.ops import (
@@ -14,19 +15,19 @@ from ...constants import SAMPLE_DIR
 
 
 @pytest.fixture
-def app():
-    config_path = Path(SAMPLE_DIR, "config.py")
-    database_uri = "sqlite:///:memory:"
+def app() -> Flask:
+    config_path = Path(SAMPLE_DIR, 'config.py')
+    database_uri = 'sqlite:///:memory:'
     app = create_app(config_path=config_path, SQLALCHEMY_DATABASE_URI=database_uri)
-    csvdir = Path(SAMPLE_DIR, "csv")
+    csvdir = Path(SAMPLE_DIR, 'csv')
     with app.app_context():
         define_tables()
         import_tables_from_csv_files(csvdir)
-    yield app
+    return app
 
 
-def test_export_db_to_csv_files(app, tmpdir_factory, snapshot):
-    outdir = str(tmpdir_factory.mktemp("csv"))
+def test_export_db_to_csv_files(app: Flask, tmpdir_factory, snapshot):
+    outdir = str(tmpdir_factory.mktemp('csv'))
 
     with app.app_context():
         export_db_to_csv_files(outdir)
